c3b0c985e67519733a1015459a47a185ddfefbc7
Jeff Dickey <jdickey@seven-sigma.com>
2014-06-09 13:13:20 +0800

    Updated BlogController#index to instantiate a Blog object.
    RSpec: 17 examples, 0 failures
    Script: 0 tests: 0 failures, 0 errors, 0 pending, 0 omitted
    RuboCop: 14 files inspected, no offenses detected
    RuboCop global overrides in `.rubocop.yml`: 0
    RuboCop local overrides in our source/spec files: 0

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -2,5 +2,6 @@
 # A controller should assign resources and redirect flow. Full stop.
 class BlogController < ApplicationController
   def index
+    @blog = Blog.new
   end
 end
--- spec/controllers/blog_controller_spec.rb
+++ spec/controllers/blog_controller_spec.rb
@@ -12,8 +12,37 @@ describe BlogController do
 
   describe "GET 'index'" do
     it 'returns http success' do
-      get 'index'
+      get :index
       response.should be_success
     end
-  end
+
+    it 'renders the index template' do
+      get :index
+      expect(response).to render_template 'index'
+    end
+
+    describe 'assigns a "blog" controller variable that' do
+
+      after :each do
+        get :index
+        blog = assigns 'blog'
+        expect(blog.send @field).to eq @expected
+      end
+
+      it 'has the expected title' do
+        @expected = 'Watching Paint Dry'
+        @field = :title
+      end
+
+      it 'has the expected subtitle' do
+        @expected = 'The trusted source for drying paint news and opinion'
+        @field = :subtitle
+      end
+
+      it 'has the expected entries' do
+        @expected = []
+        @field = :entries
+      end
+    end # describe 'assigns a "blog" controller variable that'
+  end # describe "GET 'index'"
 end
