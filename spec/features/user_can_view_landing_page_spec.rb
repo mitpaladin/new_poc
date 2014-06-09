095002d63114a0c903526848efebe9ab84a8dccd
Jeff Dickey <jdickey@seven-sigma.com>
2014-06-09 13:02:16 +0800

    Retro feature spec: landing page title/subtitle.
    RSpec: 7 examples, 0 failures
    Script: 0 tests: 0 failures, 0 errors, 0 pending, 0 omitted
    RuboCop: 12 files inspected, no offenses detected
    RuboCop global overrides in `.rubocop.yml`: 0
    RuboCop local overrides in our source/spec files: 0

---
+++ spec/features/user_can_view_landing_page_spec.rb
@@ -0,0 +1,18 @@
+
+require 'spec_helper'
+
+describe 'User can view the landing page' do
+
+  before :each do
+    visit root_path
+  end
+
+  it 'and see the title and subtitle' do
+    blog_title = 'Watching Paint Dry'
+    blog_subtitle = 'The trusted source for paint drying news and opinion'
+
+    expect(page).to have_content blog_title
+    expect(page).to have_content blog_subtitle
+  end
+
+end # describe 'User can view the landing page'
