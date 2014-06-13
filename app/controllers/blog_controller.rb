775d3f8419d4f79f8c9343760540a4285d54d936
Jeff Dickey <jdickey@seven-sigma.com>
2014-06-13 23:06:07 +0800

    Forgot to actually *use* the placeholders. Oops.
    RSpec: 61 examples, 0 failures
    Script: 0 tests: 0 failures, 0 errors, 0 pending, 0 omitted
    RuboCop: 28 files inspected, no offenses detected
    RuboCop global overrides in `.rubocop.yml`: 0
    RuboCop local overrides in our source/spec files: 0

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,9 +1,12 @@
 
 require 'blog_listing_builder'
+require 'placeholder_builder'
 
 # A controller should assign resources and redirect flow. Full stop.
 class BlogController < ApplicationController
   def index
-    @blog = DSO::BlogListingBuilder.run! blog: Blog.new
+    blog = Blog.new
+    DSO::PlaceholderBuilder.run! blog: blog
+    @blog = DSO::BlogListingBuilder.run! blog: blog
   end
 end
