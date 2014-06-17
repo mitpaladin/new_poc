f536124f668fbd7330311954547b30c344a4eded
Jeff Dickey <jdickey@seven-sigma.com>
2014-06-18 02:29:52 +0800

    BlogDataBoundary BLO now uses BlogData implementation model.
    RSpec: 64 examples, 0 failures
    Script: 0 tests: 0 failures, 0 errors, 0 pending, 0 omitted
    RuboCop: 33 files inspected, no offenses detected
    RuboCop global overrides in `.rubocop.yml`: 0
    RuboCop local overrides in our source/spec files: 0

--- app/models/blo/blog_data_boundary.rb
+++ app/models/blo/blog_data_boundary.rb
@@ -6,8 +6,10 @@ module BLO
     attr_reader :title, :subtitle
 
     def initialize(_params = {})
-      @title = 'Watching Paint Dry'
-      @subtitle = 'The trusted source for drying paint news and opinion'
+      data = BlogData.all.first
+      @title = data.title
+      @subtitle = data.subtitle
+      self
     end
   end # class BLO::BlogDataBoundary
 end # module BLO
