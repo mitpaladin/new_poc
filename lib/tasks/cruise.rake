82d8b12bf64189bb96c0f7b05844d5d25df3720e
Jeff Dickey <jdickey@seven-sigma.com>
2014-06-18 02:26:43 +0800

    Created BlogData ActiveRecord model.
    Also created database seed data and updated CI task "cruise.rake" to
    use it.
    
    RSpec: 64 examples, 0 failures
    Script: 0 tests: 0 failures, 0 errors, 0 pending, 0 omitted
    RuboCop: 33 files inspected, no offenses detected
    RuboCop global overrides in `.rubocop.yml`: 0
    RuboCop local overrides in our source/spec files: 0

--- lib/tasks/cruise.rake
+++ lib/tasks/cruise.rake
@@ -1,6 +1,6 @@
 
 task :cruise do
-  system 'rake db:reset'
+  system 'RAILS_ENV=test rake db:reset db:seed'
   # system 'rake db:test:clone'   # Don't do this for Rails 4
   # system 'teaspoon -q -f tap_y | tapout pretty' # Comment out until we have CS
   system 'rspec' # ' -p -w'
---
+++ spec/factories/blog_data.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: blog_data
+#
+#  id         :integer          not null, primary key
+#  title      :string(255)      not null
+#  subtitle   :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :blog_datum, class: 'BlogData' do
+    title 'Watching Paint Dry'
+    subtitle 'The trusted source for drying paint news & opinion'
+  end
+end
