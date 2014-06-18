41a0a3f8b3c493ebf5dc2cfc10110b1b1046859c
Jeff Dickey <jdickey@seven-sigma.com>
2014-06-18 14:41:39 +0800

    BlogDataBoundary BLO now works with/without specified record ID.
    This had been an itch in the back of my mind since writing the original
    code, and I finally recognised it and figured out how to "scratch" it.
    
    RSpec: 71 examples, 0 failures
    Script: 0 tests: 0 failures, 0 errors, 0 pending, 0 omitted
    RuboCop: 35 files inspected, no offenses detected
    RuboCop global overrides in `.rubocop.yml`: 0
    RuboCop local overrides in our source/spec files: 0

--- app/models/blo/blog_data_boundary.rb
+++ app/models/blo/blog_data_boundary.rb
@@ -5,11 +5,20 @@ module BLO
   class BlogDataBoundary
     attr_reader :title, :subtitle
 
-    def initialize(_params = {})
-      data = BlogData.all.first
+    def initialize(params = {})
+      data = get_blog_data_for params
       @title = data.title
       @subtitle = data.subtitle
       self
+    end
+
+    private
+
+    def get_blog_data_for(params = {})
+      default_id = BlogData.all.first.id
+      default_params = { id: default_id }
+      blog_params = params.fetch :blog_params, default_params
+      BlogData.find blog_params[:id]
     end
   end # class BLO::BlogDataBoundary
 end # module BLO
