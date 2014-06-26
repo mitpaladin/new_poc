60c35ae8cb740802942f7ed257fdbf577591b295
Jeff Dickey <jdickey@seven-sigma.com>
2014-06-27 01:25:41 +0800

    Added image URL to `PostCreatorAndPublisher` DSO.
    Reorganised/improved error-handling specs, and minor cleanup to
    `#execute` method.
    
    RSpec: 164 examples, 0 failures
    Script: 0 tests: 0 failures, 0 errors, 0 pending, 0 omitted
    RuboCop: 54 files inspected, no offenses detected
    RuboCop global overrides in `.rubocop.yml`: 0
    RuboCop local overrides in our source/spec files: 0

--- spec/interactors/post_creator_and_publisher_spec.rb
+++ spec/interactors/post_creator_and_publisher_spec.rb
@@ -8,12 +8,11 @@ module DSO
   describe PostCreatorAndPublisher do
 
     let(:klass) { PostCreatorAndPublisher }
+    let(:blog) { BlogData.first.to_param }
+    let(:post_params) { FactoryGirl.attributes_for :post_datum }
+    let(:params) { { post_data: post_params, blog: blog } }
 
     describe 'succeeds when called with valid parameters, such that' do
-      let(:blog) { BlogData.first.to_param }
-      let(:post_params) { FactoryGirl.attributes_for :post_datum }
-      let(:params) { { post_data: post_params, blog: blog } }
-
       it 'no error is raised' do
         expect { klass.run! params: params }.to_not raise_error
       end
@@ -33,13 +32,39 @@ module DSO
           expect(post.body).to eq post_params[:body]
         end
 
+        it 'the correct image URL' do
+          expect(post.image_url).to eq post_params[:image_url]
+        end
+
         it 'been published' do
           expect(post).to be_published
         end
       end # describe 'returns a Post instance that has'
     end # describe 'succeeds when called with valid parameters, such that'
 
-    describe 'fails when called with' do
+    describe 'reports errors when called with' do
+
+      describe 'both an empty post body and empty image URL, so that' do
+        let(:post) do
+          post_params[:body] = ''
+          post_params[:image_url] = ''
+          klass.run! params: params
+        end
+
+        it 'the post reports itself as invalid' do
+          expect(post).to_not be_valid
+        end
+
+        it 'the expected error is shown to have been detected' do
+          message = 'Body must be present if image URL is not present'
+          expect(post).to have(1).error_message
+          expect(post.error_messages).to include message
+        end
+
+        it 'the post has not been published' do
+          expect(post).to_not be_published
+        end
+      end # describe 'both an empty post body and empty image URL, so that'
     end # describe 'fails when called with'
   end # describe DSO::PostCreatorPublisher
 end # module DSO
