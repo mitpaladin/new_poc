e46a6149564802e76ef9b3c36bd56e84a622ca8f
Jeff Dickey <jdickey@seven-sigma.com>
2014-06-26 23:39:15 +0800

    Updated new-post view with image URL field.
    Entering data here doesn't actually do anything yet; next is getting the
    controller and DSOs to do their thing…but we DO have a feature spec to
    keep updated. Oh well.
    
    RSpec: 154 examples, 0 failures
    Script: 0 tests: 0 failures, 0 errors, 0 pending, 0 omitted
    RuboCop: 54 files inspected, no offenses detected
    RuboCop global overrides in `.rubocop.yml`: 0
    RuboCop local overrides in our source/spec files: 0

---
+++ spec/features/member_can_enter_image_url_on_new_post_form_spec.rb
@@ -0,0 +1,23 @@
+
+require 'spec_helper'
+
+describe 'Member can enter' do
+
+  let(:error_message)   { '1 error prevented this PostData from being saved:' }
+  let(:post_body)       { 'The Body is six feet under, nine edge down.' }
+  let(:post_title)      { 'Clear Title to This Post' }
+  let(:post_url)        { 'http://www.example.com/foo.png' }
+
+  before :each do
+    visit new_post_path
+  end
+
+  it 'image URL on New Post form' do
+    fill_in 'Title', with: post_title
+    fill_in 'Body', with: post_body
+    fill_in 'Image URL', with: post_url
+    click_on 'Create Post'
+    expect(page).to_not have_text error_message
+  end
+
+end # describe 'Member can enter'
