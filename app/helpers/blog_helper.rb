e9fcc59769fcb60addb407481e5b79118c0e9039
Jeff Dickey <jdickey@seven-sigma.com>
2014-07-02 18:52:18 +0800

    New `BlogHelper` method `#entries_for`, returns all entries for (current) blog as an array of entities (which work just fine when rendering views).
    RSpec: 206 examples, 0 failures
    Script: 0 tests: 0 failures, 0 errors, 0 pending, 0 omitted
    RuboCop: 56 files inspected, no offenses detected
    RuboCop global overrides in `.rubocop.yml`: 0
    RuboCop local overrides in our source/spec files: 0

--- app/helpers/blog_helper.rb
+++ app/helpers/blog_helper.rb
@@ -1,4 +1,7 @@
 
 # Old-style junk drawer of view-helper functions, etc.
 module BlogHelper
+  def entries_for(blog = BlogData.first)
+    BLO::BlogDataBoundary.new(blog).entries
+  end
 end
--- spec/helpers/blog_helper_spec.rb
+++ spec/helpers/blog_helper_spec.rb
@@ -11,5 +11,110 @@ require 'spec_helper'
 #   end
 # end
 describe BlogHelper do
-  # skip "add some examples to (or delete) #{__FILE__}"
+  describe '#entries_for' do
+
+    context 'without specifying a parameter' do
+
+      context 'when no entries have been added to the blog' do
+
+        it 'returns an empty array' do
+          value = helper.entries_for
+          expect(value).to be_an Array
+          expect(value).to be_empty
+        end
+      end # context 'when no entries have been added to the blog'
+
+      context 'when entries have been added to the blog' do
+        before :each do
+          FactoryGirl.create_list :post_datum, 10
+          @entries = helper.entries_for
+        end
+
+        describe 'returns an array with' do
+
+          it 'the correct number of entries' do
+            expect(@entries).to be_an Array
+            expect(@entries).to have(10).items
+          end
+
+          describe 'each item' do
+
+            it 'being a Post entity instance' do
+              @entries.each { |entry| expect(entry).to be_a Post }
+            end
+
+            describe 'having the correct' do
+
+              it 'title' do
+                expr = /Test Title Number \d+/
+                @entries.each { |entry| expect(entry.title).to match expr }
+              end
+
+              it 'body text' do
+                @entries.each { |entry| expect(entry.body).to eq 'The Body' }
+              end
+
+              it 'image URL' do
+                url = 'http://example.com/image1.png'
+                @entries.each { |entry| expect(entry.image_url).to match url }
+              end
+            end # describe 'having the correct'
+          end # describe 'each item'
+        end # describe 'returns an array with'
+      end # context 'when entries have been added to the blog' do
+    end # context 'without specifying a parameter' do
+
+    context 'when specifying a parameter' do
+
+      context 'when no entries have been added to the blog' do
+
+        it 'returns an empty array' do
+          blog = BlogData.first
+          value = helper.entries_for blog
+          expect(value).to be_an Array
+          expect(value).to be_empty
+        end
+      end # context 'when no entries have been added to the blog'
+
+      context 'when entries have been added to the blog' do
+        before :each do
+          FactoryGirl.create_list :post_datum, 10
+          blog = BlogData.first
+          @entries = helper.entries_for blog
+        end
+
+        describe 'returns an array with' do
+
+          it 'the correct number of entries' do
+            expect(@entries).to be_an Array
+            expect(@entries).to have(10).items
+          end
+
+          describe 'each item' do
+
+            it 'being a Post entity instance' do
+              @entries.each { |entry| expect(entry).to be_a Post }
+            end
+
+            describe 'having the correct' do
+
+              it 'title' do
+                expr = /Test Title Number \d+/
+                @entries.each { |entry| expect(entry.title).to match expr }
+              end
+
+              it 'body text' do
+                @entries.each { |entry| expect(entry.body).to eq 'The Body' }
+              end
+
+              it 'image URL' do
+                url = 'http://example.com/image1.png'
+                @entries.each { |entry| expect(entry.image_url).to match url }
+              end
+            end # describe 'having the correct'
+          end # describe 'each item'
+        end # describe 'returns an array with'
+      end # context 'when entries have been added to the blog' do
+    end # context 'when specifying a parameter'
+  end # describe '#entries_for'
 end
