6ca004b72e9a4054a2198aecf18545026cb2bfe1
Jeff Dickey <jdickey@seven-sigma.com>
2014-07-08 14:00:28 +0800

    BlogController#index now assigns a decorated BlogData instance to @blog.
    Labelled "Commit Six" on the Issue #25 checklist at
    http://j.mp/poci25gbts -- just in case anyone's paying attention.
    
    Pinging Issue #1 as well.
    
    RSpec: 238 examples, 0 failures
    Script: 0 tests: 0 failures, 0 errors, 0 pending, 0 omitted
    RuboCop: 70 files inspected, no offenses detected
    RuboCop global overrides in `.rubocop.yml`: 0
    RuboCop local overrides in our source/spec files: 0

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -2,7 +2,7 @@
 # A controller should assign resources and redirect flow. Full stop.
 class BlogController < ApplicationController
   def index
-    @blog = BlogData.first
+    @blog = BlogData.first.decorate
     # @blog = CCO::BlogCCO.to_entity datum
   end
 end
