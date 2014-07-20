654ce1b0ea59d088839d944d52b00d7e7d84f001
Jeff Dickey <jdickey@seven-sigma.com>
2014-07-21 00:17:41 +0800

    A challenge and a conundrum, moving Pundit into DSOs.
    This should be fully documented in the first ~60 lines of
    SessionsController; the reader is invited to peruse the (uncommented)
    code in the controller's #new method and compare it to the #execute
    method of the DSO, while keeping in mind the linked code submitted as a
    PR for Pundit (adding PunditAuthorizer).
    
    On a related track, if anyone can explain to me why the Obvious Code for
    what is now lines 26-27 does *not* Just Work, and even better, how to
    have a less egregious hack there, I would greatly appreciate it.
    
    RSpec: 389 examples, 0 failures
    RuboCop: 110 files inspected, no offenses detected

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,65 @@
 
+require 'pundit'
+require 'pundit_authorizer'
+
+module DSO
+  # Authorises the user to perform the action on an internally-generated, dummy
+  # SessionData instance. AFAICT, this should exactly reproduce the effects of
+  # the SessionsController#authorise_current_user method below, except that it
+  # doesn't seem to. Figuring out *why* is the current challenge.
+  class ActiveSessionUserAuthoriser < ActiveInteraction::Base
+    interface :current_user
+    string :action
+    include Pundit
+
+    # Compare this code to the `authorise_current_user` method in the controller
+    # below, then review the code accompanying PR 136 for Pundit; see
+    # https://github.com/billychan/pundit/commit/2c2f9a1 -- what am I missing?
+    def execute
+      active_user = policied_user
+      session_datum = SessionData.new id: 0
+      update_active_policy active_user, session_datum
+      # HACK: Calling PunditAuthorizer.new directly gives an error
+      #       'DSO::ActiveSessionUserAuthoriser::PunditAuthorizer not found'
+      #       Why? Calling it from the debugger Just Works here.
+      # binding.pry
+      auth_class = self.class.parent.parent::PunditAuthorizer
+      authorizer = auth_class.new active_user, session_datum
+      authorizer.authorize_on action
+      session_datum
+    end
+
+    private
+
+    def policied_user
+      user = current_user
+      user.instance_eval do
+        def self.policy_class
+          SessionDataPolicy
+        end
+      end
+      user
+    end
+
+    def update_active_policy(active_user, session_datum)
+      @policy = Pundit.policy(active_user, session_datum)
+    end
+  end # class DSO::ActiveSessionUserAuthoriser
+end # module DSO
+
+# ############################################################################ #
+# ############################################################################ #
+# ############################################################################ #
+
 # SessionsController: actions related to Sessions (logging in and out)
 class SessionsController < ApplicationController
   def new
     authorise_current_user
+    # @session = DSO::ActiveSessionUserAuthoriser
+    #     .run! current_user: current_user,
+    #           action: 'new'
+    # binding.pry
+    # @session
   end
 
   def create
