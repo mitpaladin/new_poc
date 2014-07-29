5a1bab6edb9055a175682e557c2efc00f09a2cce
Jeff Dickey <jdickey@seven-sigma.com>
2014-07-29 23:50:08 +0800

    Added new FeatureSpecNewPostHelper support class.
    Extracted from, and replacing, code in the "member can create valid
    image post" feature spec, this new support class also supports text
    posts.
    
    RSpec: 420 examples, 0 failures
    RuboCop: 116 files inspected, no offenses detected

--- spec/features/member_can_create_valid_image_post_spec.rb
+++ spec/features/member_can_create_valid_image_post_spec.rb
@@ -2,30 +2,25 @@
 require 'spec_helper'
 
 require 'support/feature_spec/login_helper'
+require 'support/feature_spec/new_post_helper'
 
-describe 'Member can create a valid image post' do
+describe 'Member can create a valid image post and see' do
 
   before :each do
     FeatureSpecLoginHelper.new(self).register_and_login
-    visit new_post_path
+    FeatureSpecNewPostHelper.new(self).create_image_post
   end
 
-  it 'by entering the title and body text' do
-    post_title = 'This is a Title!'
-    post_body = "Isn't this a lovely image?"
-    image_url = 'http://fc01.deviantart.net/fs70/f/2014/113/e/6/' \
-        'dreaming_of_another_reality_by_razielmb-d7fgl3s.png'
-
-    fill_in 'Title', with: post_title
-    fill_in 'Body', with: post_body
-    fill_in 'Image URL', with: image_url
-    click_on 'Create Post'
+  it 'the confirmation flash message' do
     expect(page).to have_text 'Post added!'
-    expect(page).to have_selector 'article > header > h3', post_title
+  end
+
+  it 'the newly-entered article details' do
+    expect(page).to have_selector 'article > header > h3', @post_title
     expect(page).to have_selector 'article > header > p > time'
-    expect(page).to have_selector 'article > figure > figcaption', post_body
-    selector = format 'article > figure > img[src="%s"]', image_url
+    expect(page).to have_selector 'article > figure > figcaption', @post_body
+    selector = format 'article > figure > img[src="%s"]', @image_url
     expect(page).to have_selector selector
   end
 
-end # describe 'Member can create a valid image post'
+end # describe 'Member can create a valid image post and see'
