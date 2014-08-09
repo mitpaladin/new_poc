65db726b87a977c357c47d14389d34bd564ae7d1
Jeff Dickey <jdickey@seven-sigma.com>
2014-08-10 00:25:32 +0800

    [Fix #64] Landing page has links to each post.
    Instead of just showing the post title, now shows a link to the post,
    using the title as link text.
    
    RSpec: 526 examples, 0 failures
    RuboCop: 133 files inspected, no offenses detected

---
+++ spec/features/user_can_view_article_from_landing_page_spec.rb
@@ -0,0 +1,21 @@
+
+require 'spec_helper'
+
+describe 'User can view article from landing page' do
+
+  before :each do
+    FeatureSpecLoginHelper.new(self).register_and_login
+    FeatureSpecNewPostHelper.new(self).create_image_post
+    # should be back on landing page now
+  end
+
+  it 'and see the article title link' do
+    expect(page).to have_link @post_title, post_path(@post_slug)
+  end
+
+  it 'by clicking on the article title' do
+    click_link @post_title
+    expect(page).to have_css 'dl > dd', @user_name
+  end
+
+end # describe 'User can view article from landing page'
