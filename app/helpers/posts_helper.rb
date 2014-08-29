b88a5dbf9984adfbc735f12251fe9b7207dc3450
Jeff Dickey <jdickey@seven-sigma.com>
2014-08-30 03:57:41 +0800

    Added PostsHelper#edit_post_form_attributes method
    Also extracted commonality between the two current
    `*_post_form_attributes` methods into a new method. Ready to add
    post-editing controller actions and views.
    
    RSpec: 676 examples, 0 failures (but see Issue #87)
    RuboCop: 151 files inspected, no offenses detected

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,14 +1,27 @@
 
 # Old-style junk drawer of view-helper functions, etc.
 module PostsHelper
-  def new_post_form_attributes
+  def new_post_form_attributes(_params = {})
+    attribs = shared_post_form_attributes 'new_post'
+    attribs[:url] = posts_path
+    attribs
+  end
+
+  def edit_post_form_attributes(post)
+    attribs = shared_post_form_attributes 'edit_post'
+    attribs[:url] = post_path(post)
+    attribs
+  end
+
+  private
+
+  def shared_post_form_attributes(which)
     {
-      html:   {
-        class:  'form-horizontal new_post',
-        id:     'new_post'
+      html: {
+        class:  ['form-horizontal', which].join(' '),
+        id:     which
       },
-      role:   'form',
-      url:    posts_path
+      role:     'form'
     }
   end
 end
--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -1,48 +1,65 @@
 
 require 'spec_helper'
 
-describe PostsHelper do
-  describe :new_post_form_attributes.to_s do
-    subject(:attribs) { helper.new_post_form_attributes }
+shared_examples 'a form-attributes helper' do |form_name|
 
+  describe ":#{form_name}_form_attributes" do
     it 'returns a Hash' do
       expect(subject).to be_a Hash
     end
 
     describe 'has top-level keys for' do
-      after :each do
-        expect(subject).to have_key RSpec.current_example.description.to_sym
-      end
-
-      it :html do
-      end
-
-      it :role do
-      end
-
-      it :url do
+      [:html, :role, :url].each do |key|
+        it ":#{key}" do
+          expect(subject).to have_key key
+        end
       end
     end # describe 'has top-level keys for'
 
     describe 'has an :html sub-hash that contains the correct values for' do
       it 'the :id key' do
-        expect(subject[:html][:id]).to eq 'new_post'
+        expect(subject[:html][:id]).to eq form_name
       end
 
       it 'the :class key' do
         classes = subject[:html][:class].split(/\s+/)
         expect(classes).to include 'form-horizontal'
-        expect(classes).to include 'new_post'
+        expect(classes).to include form_name
       end
     end # describe 'has an :html sub-hash that contains the correct values for'
 
     it 'has a :role item with the value "form" as an ARIA instruction' do
       expect(subject[:role]).to eq 'form'
     end
+  end # describe ":#{form_name}_form_attributes"
+end
+
+describe PostsHelper do
+  describe :new_post_form_attributes.to_s do
+    subject { helper.new_post_form_attributes }
+
+    it_behaves_like 'a form-attributes helper', 'new_post'
 
     it 'has a :url item with the value returned from the posts_path helper' do
       expected = helper.posts_path
       expect(subject[:url]).to eq expected
     end
   end # describe :new_post_form_attributes
+
+  describe :edit_post_form_attributes.to_s do
+    let(:post_data) { FactoryGirl.create :post_datum }
+    subject { helper.send :edit_post_form_attributes, post_data }
+    let(:form_name) { 'edit_post' }
+
+    it_behaves_like 'a form-attributes helper', 'edit_post'
+
+    description = 'has a :url item with the value returned from the post_path' \
+        ' helper for a specific post'
+    it description do
+      expected = helper.post_path(post_data)
+      expect(subject[:url]).to eq expected
+    end
+  end # describe :edit_post_form_attributes
+
+  # it_behaves_like 'post form-attributes helper', 'edit_post'
 end # describe PostsHelper
