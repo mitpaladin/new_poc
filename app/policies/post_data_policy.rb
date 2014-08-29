9b1f9fff8377e89840a4439bcce12eac2d7d9f88
Jeff Dickey <jdickey@seven-sigma.com>
2014-08-30 03:46:36 +0800

    Added update auth to PostData Pundit policy
    RSpec: 668 examples, 0 failures, 8 pending (but see Issue #87)
    RuboCop: 151 files inspected, no offenses detected

--- app/policies/post_data_policy.rb
+++ app/policies/post_data_policy.rb
@@ -5,4 +5,12 @@ class PostDataPolicy < ApplicationPolicy
     # Should return `user.registered?` -- but that's an *entity* concept.
     user.name != user.class.first.name  # reject the Guest User
   end
+
+  def edit?
+    record.author_name == user.name
+  end
+
+  def update?
+    edit?
+  end
 end # class PostDataPolicy
--- spec/policies/post_data_policy_spec.rb
+++ spec/policies/post_data_policy_spec.rb
@@ -5,7 +5,8 @@ describe PostDataPolicy do
   subject { PostDataPolicy }
   let(:guest_user) { UserData.first }
   let(:registered_user) { FactoryGirl.build :user_datum }
-  let(:instance) { SessionData.new }
+  let(:author) { FactoryGirl.build :user_datum }
+  let(:instance) { FactoryGirl.create :post_datum, author_name: author.name }
 
   permissions :create? do
 
@@ -17,4 +18,38 @@ describe PostDataPolicy do
       expect(subject).to permit(registered_user, instance)
     end
   end # permissions :create?
+
+  permissions :edit? do
+    describe 'prohibits invoking the :edit action by' do
+
+      it 'the Guest User' do
+        expect(subject).not_to permit(guest_user, instance)
+      end
+
+      it 'a Registered User other than the author' do
+        expect(subject).not_to permit(registered_user, instance)
+      end
+    end # describe 'prohibits invoking the :edit action by'
+
+    it 'permits the post author to invoke the :edit action' do
+      expect(subject).to permit(author, instance)
+    end
+  end # permissions :edit?
+
+  permissions :update? do
+    describe 'prohibits invoking the :update action by' do
+
+      it 'the Guest User' do
+        expect(subject).not_to permit(guest_user, instance)
+      end
+
+      it 'a Registered User other than the author' do
+        expect(subject).not_to permit(registered_user, instance)
+      end
+    end # describe 'prohibits invoking the :update action by'
+
+    it 'permits the post author to invoke the :update action' do
+      expect(subject).to permit(author, instance)
+    end
+  end # permissions :update?
 end # describe PostDataPolicy
