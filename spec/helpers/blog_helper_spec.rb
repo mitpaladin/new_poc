4b6dc53e8e8c98bc509ec4509f75d5d7914f7a6b
Jeff Dickey <jdickey@seven-sigma.com>
2014-09-05 12:20:33 +0800

    Eliminated BlogHelper#entries_for
    This also eliminated an RSpec shared_examples block which contained the
    (easily parameterisable) specs for that method.
    
    RSpec: 684 examples, 0 failures (but see Issue #87)
    RuboCop: 152 files inspected, no offenses detected

--- spec/helpers/blog_helper_spec.rb
+++ spec/helpers/blog_helper_spec.rb
@@ -1,14 +1,5 @@
 
 require 'spec_helper'
 
-require 'support/shared_examples/blog_helper/a_call_to_entries_for'
-
 describe BlogHelper do
-  describe '#entries_for' do
-
-    it_behaves_like 'a call to #entries_for with no parameters'
-
-    it_behaves_like 'a call to #entries_for with a blog parameter'
-
-  end # describe '#entries_for'
 end
