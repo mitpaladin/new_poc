5441bbdbbd0108d6264ae9beee344d351ea0ce9a
Jeff Dickey <jdickey@seven-sigma.com>
2014-09-07 23:58:21 +0800

    BlogDataDecorator discombobulated.
    As BlogData (the implementation model) is on its way out,
    removing/moving dependencies and interactions. Here, the shifted code
    was the former BlogDataDecorator#summarise method, moved (at least
    temporarily) to BlogHelper#summarise_blog.
    
    More to come.
    
    RSpec: 691 examples, 0 failures (but see Issue #87)
    RuboCop: 154 files inspected, no offenses detected

--- app/decorators/blog_data_decorator.rb
+++ app/decorators/blog_data_decorator.rb
@@ -3,26 +3,7 @@
 class BlogDataDecorator < Draper::Decorator
   delegate_all
 
-  def initialize(*)
-    super
-    @default_entry_count = 10
-  end
-
-  def summarise(count = default_entry_count)
-    PostData
-        .all
-        .map(&:decorate)
-        .select(&:published?)
-        .sort_by(&:pubdate)
-        .reverse
-        .take(count)
-  end
-
   def self.policy_class
     BlogDataPolicy
   end
-
-  protected
-
-  attr_reader :default_entry_count
 end
--- app/helpers/blog_helper.rb
+++ app/helpers/blog_helper.rb
@@ -1,4 +1,13 @@
 
 # Old-style junk drawer of view-helper functions, etc.
 module BlogHelper
+  def summarise_blog(count = 10)
+    PostData
+        .all
+        .map(&:decorate)
+        .select(&:published?)
+        .sort_by(&:pubdate)
+        .reverse
+        .take(count)
+  end
 end
