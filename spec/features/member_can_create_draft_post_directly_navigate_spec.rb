470a0ab4edc0359dce1a410c656c4c90849cec26
Jeff Dickey <jdickey@seven-sigma.com>
2014-10-01 22:00:47 +0800

    "Create draft post & navigate to it" feature spec.
    RSpec: 807 examples, 0 failures (but see Issue #87)
    RuboCop: 155 files inspected, no offenses detected

---
+++ spec/features/member_can_create_draft_post_directly_navigate_spec.rb
@@ -0,0 +1,36 @@
+
+require 'spec_helper'
+
+require 'support/feature_spec/login_helper'
+require 'support/feature_spec/new_post_helper'
+
+describe 'Member can create a draft post and' do
+
+  before :each do
+    FeatureSpecLoginHelper.new(self).register_and_login
+    data = PostHelperSupport::PostCreatorData.new post_status: 'draft'
+    FeatureSpecNewPostHelper.new(self, data).create_image_post
+  end
+
+  it 'see the confirmation flash message' do
+    expect(page).to have_text 'Post added!'
+  end
+
+  it 'directly navigate to the newly-entered post page' do
+    visit post_path(@post_title.parameterize)
+  end
+
+  it 'the newly-entered article details' do
+    visit post_path(@post_title.parameterize)
+    expect(page).to have_selector '.page-header > h1', @post_title
+    expected = %(by #{@user_name})
+    expect(page).to have_selector '.page-header > h1 > small > i', expected
+
+    # There's got to be an easier way to match Markdown content...
+    md = Redcarpet::Markdown.new Redcarpet::Render::HTML.new
+    pbody = Nokogiri.parse(md.render(@post_body)).children.first
+    # FWIW, be advised that we're matching markup converted to text here...
+    expect(page.find('figcaption')).to have_content pbody.inner_text
+  end
+
+end # describe 'Member can create a draft post and'
