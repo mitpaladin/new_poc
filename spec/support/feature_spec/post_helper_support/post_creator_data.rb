d30dc8c8a991acb9e0d9b3b263c1458d9187cb7a
Jeff Dickey <jdickey@seven-sigma.com>
2014-10-01 14:36:44 +0800

    Updated PostCreatorData to support post status.
    Initial status may be set using the hash passed to #initialize; the
    #step method (which sets body and title to the next in the sequence)
    now accepts a status string for the "new post".
    
    RSpec: 804 examples, 0 failures (but see Issue #87)
    RuboCop: 154 files inspected, no offenses detected

--- spec/support/feature_spec/post_helper_support/post_creator_data.rb
+++ spec/support/feature_spec/post_helper_support/post_creator_data.rb
@@ -4,6 +4,8 @@ require_relative '../../sequencer'
 module PostHelperSupport
   # Encapsulates/sequences data suitable for post generation.
   class PostCreatorData
+    attr_reader :post_status
+
     def initialize(params = {})
       title_format = params.fetch :post_title, 'Post Title %d'
       post_start = params.fetch :post_start, 0
@@ -11,6 +13,7 @@ module PostHelperSupport
       format_str = 'This is *another* post body. (Number %d in a series.)'
       body_format = params.fetch :post_body, format_str
       @post_body = Sequencer.new body_format, post_start
+      @post_status = force_allowed_status(params.fetch :post_status, 'public')
     end
 
     def post_body
@@ -21,9 +24,17 @@ module PostHelperSupport
       @post_title.to_s
     end
 
-    def step
+    def step(new_status = nil)
       @post_body.step
       @post_title.step
+      @post_status = force_allowed_status new_status
+    end
+
+    private
+
+    def force_allowed_status(status)
+      return status if %w(public draft).include? status
+      'public'
     end
   end # class PostHelperSupport::PostCreatorData
 end # module PostHelperSupport
--- spec/support/feature_spec/post_helper_support/post_creator_data_spec.rb
+++ spec/support/feature_spec/post_helper_support/post_creator_data_spec.rb
@@ -1,14 +1,18 @@
 
+require 'pry'
+
 require_relative 'post_creator_data'
 
 # Internal support-code module for FeatureSpecNewPostHelper class.
 module PostHelperSupport
   # Specs for class that encapsulates/sequences data for post generation.
   describe PostCreatorData do
+    let(:default_status) { 'public' }
+
     describe 'has a method returning a String or equivalent for' do
       let(:obj) { PostCreatorData.new }
 
-      [:post_title, :post_body].each do |method_sym|
+      [:post_title, :post_body, :post_status].each do |method_sym|
         it "##{method_sym}" do
           expect(obj).to respond_to method_sym
           method_ret = obj.send method_sym
@@ -18,30 +22,55 @@ module PostHelperSupport
       end
     end # describe 'has a method returning a String or equivalent for'
 
-    describe 'has a #step method that updates' do
+    describe 'has a #step method that' do
       let(:obj) { PostCreatorData.new }
-      let(:pb_format) do
-        'This is \*another\* post body\. \(Number (\d+?) in a series\.\)'
-      end
-      let(:patterns) do
-        {
-          post_title: Regexp.new(/Post Title (\d+?)/),
-          post_body:  Regexp.new(pb_format)
-        }
-      end
 
-      [:post_title, :post_body].each do |method_sym|
-        it "the internal field so that ##{method_sym} returns the next value" do
-          v1 = obj.send method_sym
-          index1 = Integer(patterns[method_sym].match(v1)[1])
-          expect(index1).to eq 1
-          obj.step
-          v2 = obj.send method_sym
-          index2 = Integer(patterns[method_sym].match(v2)[1])
-          expect(index2).to eq 2
+      describe 'updates' do
+        let(:pb_format) do
+          'This is \*another\* post body\. \(Number (\d+?) in a series\.\)'
         end
-      end
-    end # describe 'has a #step method that updates'
+        let(:patterns) do
+          {
+            post_title: Regexp.new(/Post Title (\d+?)/),
+            post_body:  Regexp.new(pb_format)
+          }
+        end
+
+        [:post_title, :post_body].each do |method_sym|
+          description = "the internal field so that ##{method_sym} returns " \
+              'the next value'
+          it description do
+            v1 = obj.send method_sym
+            index1 = Integer(patterns[method_sym].match(v1)[1])
+            expect(index1).to eq 1
+            obj.step
+            v2 = obj.send method_sym
+            index2 = Integer(patterns[method_sym].match(v2)[1])
+            expect(index2).to eq 2
+          end
+        end
+      end # describe 'updates'
+
+      describe 'accepts a parameter to use for the next instance status' do
+
+        describe 'with acceptable values of' do
+          %w(public draft).each do |status|
+            it status do
+              v1 = obj.post_status
+              expect(v1).to eq 'public'
+              obj.step status
+              v2 = obj.post_status
+              expect(v2).to eq status
+            end
+          end
+        end # describe 'with acceptable values of'
+
+        it 'ignoring illegal values and revierting to the default value' do
+          obj.step 'bogus status value'
+          expect(obj.post_status).to eq default_status
+        end
+      end # describe 'accepts a parameter to use for the next instance status'
+    end # describe 'has a #step method that'
 
     context 'by default' do
       let(:obj) { PostCreatorData.new }
@@ -97,5 +126,26 @@ module PostHelperSupport
         end
       end
     end # context 'specifying a post-start value'
+
+    describe :post_status.to_s do
+
+      it 'defaults to "public" unless otherwise specified' do
+        expect(PostCreatorData.new.post_status).to eq default_status
+      end
+
+      describe 'accepts explicit initialisation to' do
+        %w(draft public).each do |status|
+          it status do
+            obj = PostCreatorData.new post_status: status
+            expect(obj.post_status).to eq status
+          end
+        end
+      end # describe 'accepts explicit initialisation to'
+
+      it 'rejects invalid initialisation, reverting to default value' do
+        obj = PostCreatorData.new post_status: 'bogus'
+        expect(obj.post_status).to eq default_status
+      end
+    end # describe :post_status
   end # describe PostCreatorData
 end # module PostHelperSupport
