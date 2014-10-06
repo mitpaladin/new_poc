82251a715f0b7005d46513f10024e2783e5e15c9
Jeff Dickey <jdickey@seven-sigma.com>
2014-10-06 19:54:05 +0800

    Decoupled BlogSummariser from its base logic.
    New `BaseSummariser` class has all the core logic and DSL support of
    the previous `BlogSummariser`, with no application-specific (Blog- or
    PostData-related) knowledge.
    
    RSpec: 841 examples, 0 failures (but see Issue #87)
    RuboCop: 161 files inspected, no offenses detected

--- app/helpers/blog_helper/blog_summariser.rb
+++ app/helpers/blog_helper/blog_summariser.rb
@@ -1,18 +1,27 @@
 
 # Old-style junk drawer of view-helper functions, etc.
 module BlogHelper
-  # Support class for #summarise_blog method; builds list of Posts.
-  class BlogSummariser
+  # Generic "summarise" class without specific domain knowledge.
+  class BaseSummariser
     attr_writer :count
 
     def initialize(&block)
-      @count = 10
-      @aggregator = -> { PostData.all }
-      @selector = -> (data) { data }
-      @sorter = -> (data) { data.sort_by(&:pubdate) }
-      @orderer = -> (data) { data.reverse }
-      @chunker = -> (data) { data.take count }
+      @count ||= 10
+      @data_class ||= 'UNKNOWN DATA CLASS; SOME CLASS SHOULD BE ASSIGNED HERE'
+      @aggregator ||= -> { @data_class.all }
+      @selector ||= -> (data) { data }
+      @sorter ||= -> (data) { data }
+      @orderer ||= -> (data) { data }
+      @chunker ||= -> (data) { data.take @count }
       instance_eval(&block) if block
+    end
+
+    def summarise_data(data)
+      summary_steps.each do |op|
+        step = ('@' + op.to_s).to_sym
+        data = instance_variable_get(step).call data
+      end
+      data
     end
 
     def method_missing(name, *args, &block)
@@ -24,18 +33,7 @@ module BlogHelper
       super || dsl_name?(method)
     end
 
-    def summarise
-      data = PostDataDecorator.decorate_collection @aggregator.call
-      summary_steps.each do |op|
-        step = ('@' + op.to_s).to_sym
-        data = instance_variable_get(step).call data
-      end
-      data
-    end
-
     private
-
-    attr_reader :count
 
     def dsl_name?(name)
       dsl_methods.include? name
@@ -47,6 +45,22 @@ module BlogHelper
 
     def summary_steps
       dsl_methods.drop(1)
+    end
+  end # class BaseSummariser
+
+  # Support class for #summarise_blog method; builds list of Posts.
+  class BlogSummariser < BaseSummariser
+    def initialize(&block)
+      @count = 10
+      @data_class = PostData
+      @sorter = -> (data) { data.sort_by(&:pubdate) }
+      @orderer = -> (data) { data.reverse }
+      super
+    end
+
+    def summarise
+      data = PostDataDecorator.decorate_collection @aggregator.call
+      summarise_data data
     end
   end # class BlogHelper::BlogSummariser
 end # module BlogHelper
