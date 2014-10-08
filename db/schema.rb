6aa65c4dc810b4e190a4628e6f0349df90a429b3
Jeff Dickey <jdickey@seven-sigma.com>
2014-10-08 22:34:47 +0800

    Added new Posts table preparatory to DAO.
    RSpec: 822 examples, 0 failures (but see Issue #87)
    RuboCop: 154 files inspected, no offenses detected

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141007171345) do
+ActiveRecord::Schema.define(version: 20141008142656) do
 
   create_table "bootsy_image_galleries", force: true do |t|
     t.integer  "bootsy_resource_id"
@@ -49,6 +49,17 @@ ActiveRecord::Schema.define(version: 20141007171345) do
     t.datetime "pubdate"
     t.string   "author_name"
     t.string   "slug"
+  end
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.string   "author_name"
+    t.string   "slug"
+    t.text     "body"
+    t.string   "image_url"
+    t.datetime "pubdate"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "user_data", force: true do |t|
