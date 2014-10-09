1a58cb38e4245e0d9d16dcbd4d141b73d1e23563
Jeff Dickey <jdickey@seven-sigma.com>
2014-10-10 01:03:19 +0800

    Fixed bug in entity init; broke out to new class.
    Breezing right along with a deceptively green bar until we get to
    entity factories (see next commit), which were unable to create
    Post/UserEntity instances with expected instance variables. Further
    poking led to a fix, which led to breaking out common code into a new
    shared class that has been (temporarily!) stuck into the `lib`
    directory. Whee!
    
    RSpec: 849 examples, 0 failures (but see Issue #87)
    RuboCop: 161 files inspected, no offenses detected

--- app/entities/post_entity.rb
+++ app/entities/post_entity.rb
@@ -1,28 +1,25 @@
 
 require 'active_model'
+require 'instance_variable_setter'
 
 # Persistence entity-layer representation for Post. Not a domain object; used to
 # communicate across the repository/DAO boundary.
 class PostEntity
   include ActiveModel::Serializers::JSON
 
-  attr_reader :author_name,
-              :body,
-              :image_url,
-              :slug,
-              :title,
-              :pubdate,
-              :created_at,
-              :updated_at
-
   def initialize(attribs)
-    attribs.each do |k, v|
-      instance_variable_set "@#{k}".to_sym, v if can_initialise? k
-    end
+    init_attrib_keys.each { |attrib| class_eval { attr_reader attrib } }
+    InstanceVariableSetter.new(self).set attribs
   end
 
   def attributes
     instance_values.symbolize_keys
+  end
+
+  # callback used by InstanceVariableSetter
+  def init_attrib_keys
+    %w(author_name body image_url slug title pubdate created_at updated_at)
+        .map(&:to_sym)
   end
 
   # we're using FriendlyID for slugs, so...
@@ -30,10 +27,7 @@ class PostEntity
     !slug.nil?
   end
 
-  private
-
-  def can_initialise?(key)
-    %w(author_name body image_url slug title pubdate created_at updated_at)
-        .map(&:to_sym).include? key
+  def inspect
+    [self.class.name, 'with attributes', attributes.to_s].join ' '
   end
 end # class PostEntity
--- app/entities/user_entity.rb
+++ app/entities/user_entity.rb
@@ -1,5 +1,6 @@
 
 require 'active_model'
+require 'instance_variable_setter'
 
 # Persistence entity-layer representation for User. Not a domain object; used to
 # communicate across the repository/DAO boundary.
@@ -16,13 +17,18 @@ class UserEntity
               :updated_at
 
   def initialize(attribs)
-    attribs.each do |k, v|
-      instance_variable_set "@#{k}".to_sym, v if can_initialise? k
-    end
+    init_attrib_keys.each { |attrib| class_eval { attr_reader attrib } }
+    InstanceVariableSetter.new(self).set attribs
   end
 
   def attributes
     instance_values.symbolize_keys
+  end
+
+  # callback used by InstanceVariableSetter
+  def init_attrib_keys
+    %w(created_at email name password password_confirmation profile slug
+       updated_at).map(&:to_sym)
   end
 
   # we're using FriendlyID for slugs, so...
@@ -30,10 +36,7 @@ class UserEntity
     !slug.nil?
   end
 
-  private
-
-  def can_initialise?(key)
-    %w(created_at email name password password_confirmation profile slug
-       updated_at).map(&:to_sym).include? key
+  def inspect
+    [self.class.name, 'with attributes', attributes.to_s].join ' '
   end
 end # class UserEntity
