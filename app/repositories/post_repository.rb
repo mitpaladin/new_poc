f7be8ac5e667501e59aca90fbc07f1ef7f4c6631
Jeff Dickey <jdickey@seven-sigma.com>
2014-10-13 23:53:20 +0800

    Factored out Repository common class code.
    The only current difference between the code for PostRepository and
    UserRepository is the default parameters used to initialise instances
    of the two classes. This has been moved into a new common superclass,
    RepositoryBase.
    
    RSpec: 905 examples, 0 failures (but see Issue #87)
    RuboCop: 179 files inspected, no offenses detected

--- app/repositories/post_repository.rb
+++ app/repositories/post_repository.rb
@@ -1,70 +1,7 @@
 
 # Intermediary between engine-bound DAO and Entity for Post-related use cases.
-class PostRepository
+class PostRepository < RepositoryBase
   def initialize(factory = PostFactory, dao = PostDao)
-    @factory, @dao = factory, dao
-  end
-
-  def add(entity)
-    record = dao.new entity.attributes
-    return successful_result(record) if record.save
-    failed_result_with_errors record.errors
-  end
-
-  def all
-    dao.all.map { |record| factory.create record }
-  end
-
-  def delete(slug)
-    # We pass in a slug; the Repository (and Entity) don't know what an 'id'
-    # is. The DAO, however, is ActiveRecord, and seems to need an ID
-    record = dao.find_by_slug(slug)
-    return failed_result(slug) unless record
-    destroyed_record_count = dao.delete(record.id)
-    # This should never happen; if we can see it, we should be able to delete it
-    return failed_result(record.id) if destroyed_record_count.zero?
-    successful_result
-  end
-
-  def find_by_slug(slug)
-    found_post = dao.where(slug: slug).first
-    return successful_result(found_post) if found_post
-    failed_result slug
-  end
-
-  def update(entity)
-    result = find_by_slug entity.slug
-    return result unless result.success?
-
-    record = dao.where(slug: entity.slug).first
-    unless record.update_attributes(entity.attributes)
-      return failed_result_with_errors record.errors
-    end
-    successful_result record
-  end
-
-  private
-
-  attr_reader :dao, :factory
-
-  def entity_if_record?(record)
-    return nil unless record
-    factory.create record
-  end
-
-  def failed_result_with_errors(errors)
-    StoreResult.new entity: nil, success: false,
-                    errors: ErrorFactory.create(errors)
-  end
-
-  def failed_result(slug)
-    errors = ActiveModel::Errors.new dao
-    errors.add :base, "A record with 'slug'=#{slug} was not found."
-    failed_result_with_errors errors
-  end
-
-  def successful_result(record = nil)
-    StoreResult.new success: true, errors: nil,
-                    entity: entity_if_record?(record)
+    super factory, dao
   end
 end # class PostRepository
--- app/repositories/user_repository.rb
+++ app/repositories/user_repository.rb
@@ -1,70 +1,7 @@
 
 # Intermediary between engine-bound DAO and Entity for User-related use cases.
-class UserRepository
+class UserRepository < RepositoryBase
   def initialize(factory = UserFactory, dao = UserDao)
-    @factory, @dao = factory, dao
-  end
-
-  def add(entity)
-    record = dao.new entity.attributes
-    return successful_result(record) if record.save
-    failed_result_with_errors record.errors
-  end
-
-  def all
-    dao.all.map { |record| factory.create record }
-  end
-
-  def delete(slug)
-    # We pass in a slug; UserRepository (and UserEntity) don't know what an 'id'
-    # is. UserDao, however, is ActiveRecord, and seems to need an ID
-    record = dao.find_by_slug(slug)
-    return failed_result(slug) unless record
-    destroyed_record_count = dao.delete(record.id)
-    # This should never happen; if we can see it, we should be able to delete it
-    return failed_result(record.id) if destroyed_record_count.zero?
-    successful_result
-  end
-
-  def find_by_slug(slug)
-    found_user = dao.where(slug: slug).first
-    return successful_result(found_user) if found_user
-    failed_result slug
-  end
-
-  def update(entity)
-    result = find_by_slug entity.slug
-    return result unless result.success?
-
-    record = dao.where(slug: entity.slug).first
-    unless record.update_attributes(entity.attributes)
-      return failed_result_with_errors record.errors
-    end
-    successful_result record
-  end
-
-  private
-
-  attr_reader :dao, :factory
-
-  def entity_if_record?(record)
-    return nil unless record
-    factory.create record
-  end
-
-  def failed_result_with_errors(errors)
-    StoreResult.new entity: nil, success: false,
-                    errors: ErrorFactory.create(errors)
-  end
-
-  def failed_result(slug)
-    errors = ActiveModel::Errors.new dao
-    errors.add :base, "A record with 'slug'=#{slug} was not found."
-    failed_result_with_errors errors
-  end
-
-  def successful_result(record = nil)
-    StoreResult.new success: true, errors: nil,
-                    entity: entity_if_record?(record)
+    super factory, dao
   end
 end
