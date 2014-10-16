9f4645f56190e1090b4df8f173c35763702b0096
Jeff Dickey <jdickey@seven-sigma.com>
2014-10-16 23:56:58 +0800

    SessionDestroyAction DSO.
    Spec includes a couple of shared examples that I should have written
    earlier, so I don't have to go back and back port them all in. :-P
    
    RSpec: 928 examples, 0 failures (but see Issue #87, including
    now-latest comment)
    RuboCop: 224 files inspected, no offenses detected

---
+++ spec/interactors/session_destroy_action_spec.rb
@@ -0,0 +1,25 @@
+
+require 'spec_helper'
+
+require 'session_destroy_action'
+require_relative 'shared_examples/a_storeresult_with_a_guest_user_entity'
+require_relative 'shared_examples/a_successful_storeresult'
+
+# Module DSO2 contains our second-generation Domain Service Objects, aka
+#   "interactors".
+module DSO2
+  describe SessionDestroyAction, outer: true do
+    let(:klass) { SessionDestroyAction }
+
+    it 'has :run and :run! methods that require no parameters' do
+      expect { klass.run! }.not_to raise_error
+    end
+
+    describe 'returns a StoreResult instance with' do
+      let(:result) { klass.run! }
+
+      it_behaves_like 'a successful StoreResult'
+      it_behaves_like 'a StoreResult with a Guest User entity'
+    end # describe 'returns a StoreResult instance with'
+  end # describe DSO2::SessionNewAction
+end # module DSO2
