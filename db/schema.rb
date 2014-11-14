687e42011ae0b76425ee1bd2f5ce2acd82cb815f
Jeff Dickey <jdickey@seven-sigma.com>
2014-11-14 14:41:43 +0800

    Ripping out PostData (3 of 3).
    She's dead, Jim.
    
    RSpec: 838 examples, 0 failures, 32 pending
    RuboCop: 188 files inspected, no offenses detected

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141113191112) do
+ActiveRecord::Schema.define(version: 20141114063522) do
 
   create_table "bootsy_image_galleries", force: true do |t|
     t.integer  "bootsy_resource_id"
@@ -39,17 +39,6 @@ ActiveRecord::Schema.define(version: 20141113191112) do
   add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
   add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
   add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
-
-  create_table "post_data", force: true do |t|
-    t.string   "title",       null: false
-    t.text     "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "image_url"
-    t.datetime "pubdate"
-    t.string   "author_name"
-    t.string   "slug"
-  end
 
   create_table "posts", force: true do |t|
     t.string   "title"
