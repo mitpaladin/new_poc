a661907cb6f0db5de91cb55f2cd5662f4986310e
Jeff Dickey <jdickey@seven-sigma.com>
2014-11-14 22:26:58 +0800

    Re-enabled "published post not editable by others"
    Feature spec now expects more specifically informative messages than
    the old generic Pundit "not authorised" messages. Good riddance.
    
    RSpec: 806 examples, 0 failures, 25 pending
    RuboCop: 186 files inspected, no offenses detected

--- spec/features/post_only_editable_by_author_spec.rb
+++ spec/features/post_only_editable_by_author_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 require 'support/feature_spec/login_helper'
 require 'support/feature_spec/new_post_helper'
 
-shared_examples 'cannot edit the post' do
+shared_examples 'cannot edit the post' do |expected|
   it 'since no "Edit <article title>" button is shown' do
     visit post_path(@post_slug)
     text = "Edit '#{@post_title}'"
@@ -13,13 +13,13 @@ shared_examples 'cannot edit the post' do
 
   it 'by directly visiting the edit-post path' do
     visit edit_post_path(@post_slug)
-    expected = 'You are not authorized to perform this action.'
+    expected ||= "User #{@user_name} is not the author of this post!"
     selector = 'div.alert.alert-error.alert-dismissable'
     expect(page).to have_selector selector, text: expected
   end
 end
 
-xdescribe 'A published post is' do
+describe 'A published post is' do
 
   before :each do
     @login_helper = FeatureSpecLoginHelper.new(self)
@@ -35,7 +35,8 @@ xdescribe 'A published post is' do
     end
 
     describe 'the Guest User' do
-      it_behaves_like 'cannot edit the post'
+      expected = 'Not logged in as a registered user!'
+      it_behaves_like 'cannot edit the post', expected
     end # describe 'the Guest User'
 
     describe 'a user other than the author' do
