8486a8a327076eb633ebc92355cde206f240852d
Jeff Dickey <jdickey@seven-sigma.com>
2014-11-20 02:11:55 +0800

    [Fix #69] View template/helper cleanup.
    Three lines of markup and helper calls in `app/views/users/show.slim`
    replaced by a single-line call to a single helper. Improvement; the
    previous template code conveyed irrelevant detail.
    
    To make *that* work, the `ProfileBioRowBuilder#to_html` method was
    fundamentally redone. No longer directly using Rails `content_tag` and
    `concat` helper methods to sling HTML together, it now uses Nokogiri; a
    much more predictable way to do the job.
    
    All this also meant that the `UsersHelper` methods
    `#profile_bio_header` and `#profile_bio_panel` are no longer needed by
    view code, so they've been eliminated. Much better, yes?
    
    RSpec: 762 examples, 0 failures
    RuboCop: 174 files inspected, no offenses detected

--- app/helpers/users_helper/profile_bio_row_builder.rb
+++ app/helpers/users_helper/profile_bio_row_builder.rb
@@ -6,11 +6,24 @@ class ProfileBioRowBuilder
     @user_name, @user_profile, @h = user_name, user_profile, h
   end
 
+  # NOTE: PAY ATTENTION! WTF WORKAROUND IN PROGRESS!
+  # This started out as a fairly conventional Rails view-helper method, with the
+  # outer block below appearing as:
+  #     h.content_tag :div, nil, { class: 'row' }, false do
+  #       # innards pretty much as is
+  #     end
+  # The WTF is that, despite the very explicit `false` parameter to #content_tag
+  # (which should disable escaping), the content added by the block WAS escaped
+  # (and thus invalid as HTML).
+  #
+  # Nokogiri, the Swiss Army Nuclear Ginsu Chainsaw, to the rescue!
   def to_html
-    h.content_tag :div, nil, { class: 'row' }, false do
-      h.concat build_profile_heading
-      h.concat ProfileBioPanelBuilder.new(user_profile, h).to_html
+    outer_div = outer_container do |div|
+      div['class'] = 'row'
+      div << ProfileBioHeaderBuilder.new(user_name, h).to_html
+      div << ProfileBioPanelBuilder.new(user_profile, h).to_html
     end
+    reformat_html_output outer_div
   end
 
   protected
@@ -19,9 +32,12 @@ class ProfileBioRowBuilder
 
   private
 
-  def build_profile_heading
-    h.content_tag :h1 do
-      "Profile Page for #{user_name}"
-    end
+  def outer_container
+    doc = Nokogiri::HTML::Document.new
+    Nokogiri::XML::Element.new('div', doc).tap { |div| yield div }.to_html
+  end
+
+  def reformat_html_output(markup)
+    markup.lines.each(&:strip!).join
   end
 end
