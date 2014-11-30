3ddc8e633c76efec07448d780fcc85f7a437f434
Jeff Dickey <jdickey@seven-sigma.com>
2014-12-01 00:47:59 +0800

    Fixed a bobbled timestamp comparison.
    "But it works on *my* machine!" :-P
    
    RSpec: 763 examples, 0 failures
    RuboCop: 178 files inspected, no offenses detected

--- spec/actions/show_post_spec.rb
+++ spec/actions/show_post_spec.rb
@@ -17,8 +17,11 @@ shared_examples 'a successfully-retrieved post' do
     end
 
     it 'is a PostEntity with correct attributes' do
-      target_post.attributes.each do |attrib, value|
-        expect(payload[attrib]).to eq value
+      attrib_keys = target_post.attributes.keys.reject { |k| k == :pubdate }
+      attrib_keys.each { |key| expect(payload[key]).to eq target_post[key] }
+      if target_post.attributes.key? :pubdate
+        expect(payload[:pubdate]).to be_within(0.5.seconds)
+          .of target_post[:pubdate]
       end
     end
   end # describe 'is successful, broadcasting a payload which'
