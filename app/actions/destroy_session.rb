a83440ba641dc2153b90fad7a2e52ba0b42cfa74
Jeff Dickey <jdickey@seven-sigma.com>
2014-12-04 00:58:57 +0800

    DestroySession no longer uses StoreResult.
    RSpec: 735 examples, 0 failures
    RuboCop: 178 files inspected, no offenses detected

--- app/actions/destroy_session.rb
+++ app/actions/destroy_session.rb
@@ -4,15 +4,11 @@ module Actions
   class DestroySession
     include Wisper::Publisher
 
-    def initialize
-    end
-
     # No-op for now. We *could* verify that the current user isn't the Guest
     # User, but YAGNI until we do (and until we pass the current user identifier
     # into the #initialize method).
     def execute
-      result = StoreResult.new success: true, errors: [], entity: nil
-      broadcast_success result
+      broadcast_success :success
     end
 
     private
