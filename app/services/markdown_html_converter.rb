f73be596af46b3bb9ad38593c3a57ece640f8f24
Jeff Dickey <jdickey@seven-sigma.com>
2014-12-18 20:07:51 +0800

    Refactoring MarkdownHtmlConverter.
    Isolating knowledge of RedCarpet(+Rouge) API in internal module, before
    developing *parallel* module using new HTML::Pipeline code.
    
    RSpec: 707 examples, 0 failures
    RuboCop: 181 files inspected, no offenses detected

--- app/services/markdown_html_converter.rb
+++ app/services/markdown_html_converter.rb
@@ -6,31 +6,39 @@ class MarkdownHtmlConverter
   # `fragment` is Markdown, HTML or some combination thereof; run it through
   # RedCarpet's Markdown parser to yield HTML to return to caller.
   def to_html(fragment)
-    # Do explicit String cast to convert any incoming nil to empty string.
-    create_renderer.render String(fragment)
+    render_using_redcarpet fragment
   end
 
   private
 
-  # Redcarpet Markdown output renderer. Uses Rouge for syntax highlighting.
-  class Renderer < Redcarpet::Render::HTML
-    include Rouge::Plugins::Redcarpet
-  end
+  # Old RedCarpet/Rouge-based code for Markdown parsing, etc. To be replaced.
+  module RedCarpetConverter
+    # Redcarpet Markdown output renderer. Uses Rouge for syntax highlighting.
+    class Renderer < Redcarpet::Render::HTML
+      include Rouge::Plugins::Redcarpet
+    end
 
-  def conversion_options
-    {
-      autolink:                     true,
-      fenced_code_blocks:           true,
-      highlight:                    true,
-      no_intra_emphasis:            false,
-      strikethrough:                true,
-      superscript:                  true,
-      tables:                       true,
-      underline:                    true
-    }
-  end
+    def conversion_options
+      {
+        autolink:                     true,
+        fenced_code_blocks:           true,
+        highlight:                    true,
+        no_intra_emphasis:            false,
+        strikethrough:                true,
+        superscript:                  true,
+        tables:                       true,
+        underline:                    true
+      }
+    end
 
-  def create_renderer
-    Redcarpet::Markdown.new Renderer, conversion_options
+    def create_renderer
+      Redcarpet::Markdown.new Renderer, conversion_options
+    end
+  end # module MarkdownHtmlConverter::RedCarpetConverter
+
+  def render_using_redcarpet(fragment)
+    extend RedCarpetConverter
+    # Do explicit String cast to convert any incoming nil to empty string.
+    create_renderer.render String(fragment)
   end
-end
+end # class MarkdownHtmlConverter
