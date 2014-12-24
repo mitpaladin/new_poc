e1cb7608afa61682c301ae03faa3fc78977b9e6a
Jeff Dickey <jdickey@seven-sigma.com>
2014-12-24 12:49:17 +0800

    Fixed *two* bugs in InstanceVariableSetter.
    Also killed `InstanceVariableSetter#allow_attributes`. It's a feature
    that (as it now turns out) our two uses of this class never use; both
    use what the spec calls callback initialisation.
    
    Next up: killing one of those two uses; namely, that in `UserEntity`.
    
    RSpec: 707 examples, 0 failures
    RuboCop: 181 files inspected, no offenses detected

--- lib/instance_variable_setter.rb
+++ lib/instance_variable_setter.rb
@@ -4,6 +4,22 @@
 # Example:
 #
 # class Foo
+#   attr_reader :foo, :bar, :baz, :quux
+#   def initialize(attrs = {})
+#     InstanceVariableSetter.new(self).set attrs
+#   end
+#   # ...
+#   private
+#   # ...
+#   def init_attrib_keys
+#     [:foo, :bar] # or %w(foo bar)
+#   end
+# end
+#
+# Previous instantiation example *that justified the existence of the class*.
+# This usage is NO LONGER SUPPORTED.
+#
+# class Foo
 #   attr_reader :foo, :bar
 #   def initialize(attrs = {})
 #     InstanceVariableSetter.new(self) do
@@ -14,36 +30,25 @@
 #   # ...
 # end
 # ...
-# the_foo = Foo.new foo: 'hello', bar: 21
+#
+# Instantiation of client class (in either case)
+# the_foo = Foo.new foo: 'hello', bar: 21, baz: 'nothing here'
 # puts the_foo.foo
 # # "hello"
-#
-# Alternate example class and usage:
-#
-# class Foo
-#   attr_reader :foo, :bar
-#   def initialize(attrs = {})
-#     InstanceVariableSetter.new(self).set attrs
-#   end
-#   # ...
-#   def init_attrib_keys
-#     %w(foo bar) # or [:foo, :bar]
-#   end
-# end
-#
+# puts the_foo.baz
+# # nil
+# puts the_foo.instance_variables
+# [:foo, :bar]
+# (Note the omission of :baz, specified in the initialiser call.)
 class InstanceVariableSetter
-  def initialize(dest_obj, &block)
+  def initialize(dest_obj)
     @dest_obj = dest_obj
     @allowed_attributes = initial_allowed_attributes
-    instance_eval(&block) if block
-  end
-
-  def allow_attributes(attribs)
-    @allowed_attributes = attribs.map(&:to_sym)
   end
 
   def set(attribs)
     allowed_attributes.each { |attrib_sym| set_ivar_for attrib_sym, attribs }
+    self
   end
 
   private
@@ -51,8 +56,8 @@ class InstanceVariableSetter
   attr_reader :allowed_attributes, :dest_obj
 
   def initial_allowed_attributes
-    return [] unless dest_obj.respond_to? :init_attrib_keys
-    dest_obj.init_attrib_keys.map(&:to_sym)
+    return [] unless dest_obj.respond_to? :init_attrib_keys, true
+    dest_obj.send(:init_attrib_keys).map(&:to_sym)
   end
 
   def ivar_for(sym)
@@ -63,5 +68,6 @@ class InstanceVariableSetter
     value = attribs[key] || attribs[key.to_s]
     return if value.nil?
     dest_obj.instance_variable_set ivar_for(key), value
+    self
   end
 end
--- spec/lib/instance_variable_setter_spec.rb
+++ spec/lib/instance_variable_setter_spec.rb
@@ -2,18 +2,6 @@
 require 'spec_helper'
 require 'instance_variable_setter'
 
-# Test class to demonstrate InstanceVariableSetter (scenario 1)
-class TestClassWithInitBlock
-  attr_reader :foo, :bar
-  def initialize(attrs = {})
-    InstanceVariableSetter.new(self) do
-      allow_attributes %w(foo bar)
-      # or: allow_attributes [:foo, :bar]
-      set attrs
-    end
-  end
-end
-
 # Test class to demonstrate InstanceVariableSetter (scenario 2)
 class TestClassWithCallback
   attr_reader :foo, :bar
@@ -27,19 +15,10 @@ class TestClassWithCallback
 end
 
 describe InstanceVariableSetter do
-  describe 'can be initialised using a' do
-    after :each do
-      expect(@test_obj.foo).to eq 'Hello'
-      expect(@test_obj.bar).to eq 21
-      expect(@test_obj.instance_variables).to eq [:@foo, :@bar]
-    end
-
-    it 'block' do
-      @test_obj = TestClassWithInitBlock.new foo: 'Hello', bar: 21
-    end
-
-    it 'callback' do
-      @test_obj = TestClassWithCallback.new foo: 'Hello', bar: 21
-    end
-  end # describe 'can be initialised using a'
+  it 'can be initialised using a callback' do
+    @test_obj = TestClassWithCallback.new foo: 'Hello', bar: 21
+    expect(@test_obj.foo).to eq 'Hello'
+    expect(@test_obj.bar).to eq 21
+    expect(@test_obj.instance_variables).to eq [:@foo, :@bar]
+  end
 end # describe InstanceVaribleSetter
