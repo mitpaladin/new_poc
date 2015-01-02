77b7cd55e74117bf462e01a09509108a2a9b17a3
Jeff Dickey <jdickey@seven-sigma.com>
2015-01-02 17:13:11 +0800

    Added support/internal classes.
    `Newpoc::Repository::Internal::ErrorFactory` adapted from
    `MelddRepository`'s `ErrorFactory` class and
    `Newpoc::Repository::Internal::StoreResult` adapted from
    `MelddRepository`'s `StoreResult` class. Next up: to actually *use*
    them.
    
    RSpec: 658+32+7 examples, 0 failures
    RuboCop: 192+10+8 files inspected, no offenses detected

--- lib/newpoc-repository-base/lib/newpoc/repository/base/version.rb
+++ lib/newpoc-repository-base/lib/newpoc/repository/base/version.rb
@@ -2,7 +2,7 @@ module Newpoc
   module Repository
     # Module containing base class of all Repository classes for app.
     module Base
-      VERSION = '0.0.1'
+      VERSION = '0.0.2'
     end
   end
 end
