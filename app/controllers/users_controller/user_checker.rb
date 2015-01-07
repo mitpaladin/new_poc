86a595beaa62103221a397357f094768bfff4e15
Charles Du <charles.q.du@theprolog.com>
2015-01-07 23:41:54 +0800

    refactored padded_filtered_msgs to cycle through all the applicable messages instead of only adding the first one to user.errors

--- app/controllers/users_controller/user_checker.rb
+++ app/controllers/users_controller/user_checker.rb
@@ -27,23 +27,29 @@ class UsersController < ApplicationController
         def check_for_invalid_passwords
           @filtered_msgs = data[:messages].grep(/password/)
           return if filtered_msgs.empty?
-          user.errors.add :password, padded_filtered_msgs
+          filtered_msgs.each do
+            |msg| user.errors.add :password, padded_filtered_msgs(msg)
+          end
         end
 
         def check_for_conflicting_name
           @filtered_msgs = data[:messages].grep(/already exists/)
           return if filtered_msgs.empty?
-          user.errors.add :name, padded_filtered_msgs
+          filtered_msgs.each do
+            |msg| user.errors.add :name, padded_filtered_msgs(msg)
+          end
         end
 
         def check_for_other_name_issues
           @filtered_msgs = data[:messages].grep(/Name/)
           return if filtered_msgs.empty?
-          user.errors.add :name, padded_filtered_msgs
+          filtered_msgs.each do
+            |msg| user.errors.add :name, padded_filtered_msgs(msg)
+          end
         end
 
-        def padded_filtered_msgs
-          'is invalid: ' + filtered_msgs.first
+        def padded_filtered_msgs(messg)
+          'is invalid: ' + messg
         end
 
         attr_reader :controller, :data, :user, :filtered_msgs
