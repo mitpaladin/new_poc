26cd98701954f7fa117dc0c78a2fabbf3193a36c
Jeff Dickey <jdickey@seven-sigma.com>
2015-01-17 02:43:39 +0800

    Inadvertently omitted changes to `bin/rakeall.sh`.
    RSpec: 590+43+34+30 examples, 0 failures
    RuboCop: 196+14+8+5 files inspected, no offenses detected

--- bin/rakeall.sh
+++ bin/rakeall.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 function rakeall() {
-  bundle exec rake
+  bundle exec rake || return 1
   for rakefile in `find lib -name Rakefile`
   do
     pushd `dirname $rakefile`
     bundle install
-    bundle exec rake
+    bundle exec rake || return 1
     popd
   done
 }
