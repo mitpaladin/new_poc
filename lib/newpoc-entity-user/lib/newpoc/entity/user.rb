613ed7940b050d43e5537ce66011cf496117cf45
Jeff Dickey <jdickey@seven-sigma.com>
2015-01-18 00:05:19 +0800

    Added `#attributes` and `#[]` methods.
    RSpec: 593+43+4+34+30 (704) examples, 0 failures
    RuboCop: 200+14+4+8+5 (231) files inspected, no offenses detected

--- lib/newpoc-entity-user/lib/newpoc/entity/user.rb
+++ lib/newpoc-entity-user/lib/newpoc/entity/user.rb
@@ -5,7 +5,35 @@ module Newpoc
   module Entity
     # Domain entity for a user of the system.
     class User
-      def initialize(_attribs)
+      attr_reader :email,
+                  :name,
+                  :profile,
+                  :slug,
+                  :created_at,
+                  :updated_at
+
+      def initialize(attribs)
+        @name = attribs[:name]
+        @email = attribs[:email]
+        @slug = attribs[:slug]
+        @updated_at = attribs[:updated_at]
+        @profile = attribs.fetch :profile, ''
+        @created_at = attribs.fetch :created_at, Time.now
+      end
+
+      def attributes
+        {
+          email: email,
+          name: name,
+          profile: profile,
+          slug: slug,
+          created_at: created_at,
+          updated_at: updated_at
+        }
+      end
+
+      def [](key)
+        attributes[key]
       end
     end # class Newpoc::Entity::User
   end
--- lib/newpoc-entity-user/spec/newpoc/entity/user_spec.rb
+++ lib/newpoc-entity-user/spec/newpoc/entity/user_spec.rb
@@ -1,12 +1,11 @@
+
 require 'spec_helper'
 
 describe Newpoc::Entity::User do
   let(:valid_subset) do
     {
       name: 'Joe Blow',
-      email: 'joe@example.com',
-      password: 'password',
-      password_confirmation: 'password_confirmation'
+      email: 'joe@example.com'
     }
   end
 
@@ -20,5 +19,37 @@ describe Newpoc::Entity::User do
         expect { described_class.new valid_subset }.not_to raise_error
       end
     end # describe 'succeeding'
+
   end # describe 'supports initialisation'
+
+  # begin 'it has initialiser-set attributes'
+  describe '#attributes' do
+    let(:obj) { described_class.new valid_subset }
+    let(:actual) { obj.attributes }
+
+    it 'returns the attributes passed to the initialiser' do
+      valid_subset.each_pair do |attrib, value|
+        expect(obj.send attrib).to eq value
+      end
+    end
+
+    it 'has nil values for all attributes not passed to the initialiser' do
+      actual.keys.reject { |k| valid_subset.key? k }.each do |attrib|
+        expect(obj[attrib.to_s]).to be nil
+      end
+    end
+  end # describe '#attributes'
+
+  describe '#[]' do
+    let(:obj) { described_class.new valid_subset }
+
+    it 'returns the correct value for an attribute by hash index' do
+      expect(obj[:name]).to eq obj.attributes[:name]
+    end
+
+    it 'returns nil for a requested attribute that does not exist' do
+      expect(obj[:bogus]).to be nil
+    end
+  end # describe '#[]'
+  # end 'it has initialiser-set attributes'
 end # describe Newpoc::Entity::User
