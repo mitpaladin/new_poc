54d2f8c277c2ea4715fa40a6d9281351ba3fe160
Jeff Dickey <jdickey@seven-sigma.com>
2015-01-31 16:28:53 +0800

    Scaffolded `newpoc-action-session-destroy` Gem.
    Just add code.
    
    RSpec: 574+2+5+39+24+34+30 (708) examples, 0 failures
    RuboCop: 199+4+4+12+5+8+5 (237) files inspected, no offenses detected

---
+++ lib/newpoc-action-session-destroy/lib/newpoc/action/session/destroy.rb
@@ -0,0 +1,13 @@
+
+require 'newpoc/action/session/destroy/version'
+
+module Newpoc
+  module Action
+    module Session
+      # Domain/business-logic action for logging out a user.
+      class Destroy
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/newpoc-action-session-destroy/spec/newpoc/action/session/destroy_spec.rb
@@ -0,0 +1,12 @@
+
+require 'spec_helper'
+
+describe Newpoc::Action::Session::Destroy do
+  it 'has a version number' do
+    expect(Newpoc::Action::Session::Destroy::VERSION).not_to be nil
+  end
+
+  xit 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
