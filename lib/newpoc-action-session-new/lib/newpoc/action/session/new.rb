0acd7f0e0d3a1d7211646c6795268e24dc17cd3a
Jeff Dickey <jdickey@seven-sigma.com>
2015-02-02 18:11:00 +0800

    Scaffolded `newpoc-action-session-new` Gem.
    RSpec: 555+11+5+2+2+5+39+24+34+30 (707) examples, 0 failures
    RuboCop: 208+4+4+4+4+4+12+5+8+5 (258) files inspected, no offenses

---
+++ lib/newpoc-action-session-new/lib/newpoc/action/session/new.rb
@@ -0,0 +1,13 @@
+
+require 'newpoc/action/session/new/version'
+
+module Newpoc
+  module Action
+    module Session
+      # New-session login encapsulation for `new_poc`.
+      class New
+        # Your code goes here...
+      end # end class Newpoc::Action::Session::New
+    end
+  end
+end
