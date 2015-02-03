79c07084f3b7e85768d60bda5aa5aa61f6b16847
Jeff Dickey <jdickey@seven-sigma.com>
2015-02-04 03:03:05 +0800

    Added `newpoc-action-user-show` code/specs.
    RSpec: 541+11+9+5+2+5+5+5+9+39+24+34+30 (719) examples, 0 failures
    RuboCop: 217+4+4+4+4+4+4+4+4+12+5+8+5 (279) files examined, no offenses

--- lib/newpoc-action-user-show/lib/newpoc/action/user/show.rb
+++ lib/newpoc-action-user-show/lib/newpoc/action/user/show.rb
@@ -1,4 +1,7 @@
 
+require 'wisper'
+require 'wisper_subscription'
+
 require 'newpoc/action/user/show/version'
 
 module Newpoc
@@ -6,8 +9,42 @@ module Newpoc
     module User
       # Retrieves a user entity from the repository; part of `new_poc`.
       class Show
-        # Your code goes here...
-      end
+        include Wisper::Publisher
+
+        def initialize(target_slug, user_repository, options = {})
+          @target_slug = target_slug
+          @user_repository = user_repository
+          @success_event = options.fetch :success, :success
+          @failure_event = options.fetch :failure, :failure
+        end
+
+        def execute
+          validate_slug
+          broadcast_success entity
+        rescue RuntimeError
+          broadcast_failure target_slug
+        end
+
+        private
+
+        attr_reader :entity, :failure_event, :success_event, :target_slug,
+                    :user_repository
+
+        def broadcast_success(payload)
+          broadcast success_event, payload
+        end
+
+        def broadcast_failure(payload)
+          broadcast failure_event, payload
+        end
+
+        def validate_slug
+          result = user_repository.find_by_slug target_slug
+          @entity = result.entity
+          return if result.success?
+          fail target_slug
+        end
+      end # end class Newpoc::Action::User::Show
     end
   end
 end
--- lib/newpoc-action-user-show/spec/spec_helper.rb
+++ lib/newpoc-action-user-show/spec/spec_helper.rb
@@ -6,4 +6,8 @@ end
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
+require 'awesome_print'
+require 'fancy-open-struct'
+require 'pry'
+
 require 'newpoc/action/user/show'
