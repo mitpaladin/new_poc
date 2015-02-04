a7783c0e2a39045a40c0e0c806d5ee973a169f4d
Jeff Dickey <jdickey@seven-sigma.com>
2015-02-04 11:51:40 +0800

    Scaffolded `newpoc-action-post-show` "Gem".
    RSpec: 537+11+9+2+5+2+5+5+5+9+39+24+34+30 (717) examples, 0 failures
    RuboCop: 220+4+4+4+4+4+4+4+4+4+12+5+8+5 (286) files inspected, no
    offenses detected

---
+++ lib/newpoc-action-post-show/lib/newpoc/action/post/show.rb
@@ -0,0 +1,13 @@
+
+require 'newpoc/action/post/show/version'
+
+module Newpoc
+  module Action
+    module Post
+      # Retrieves a Post on behalf of current user, for `new_poc`.
+      class Show
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/newpoc-action-post-show/spec/newpoc/action/post/show_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Newpoc::Action::Post::Show do
+  it 'has a version number' do
+    expect(Newpoc::Action::Post::Show::VERSION).not_to be nil
+  end
+
+  xit 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
