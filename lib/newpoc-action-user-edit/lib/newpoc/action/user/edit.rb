4570b80f596dafb1db36d03fbc3a207d1730bce5
Jeff Dickey <jdickey@seven-sigma.com>
2015-02-04 21:58:17 +0800

    Addeed `newpoc-action-user-edit` code and specs.
    RSpec: 529+11+9+9+5+2+5+9+5+5+9+39+24+34+30 (725) examples, 0 failures
    RuboCop: 223+4+4+4+4+4+4+4+4+4+4+12+5+8+5 (293) files inspected, no
    offenses detected

--- lib/newpoc-action-user-edit/lib/newpoc/action/user/edit.rb
+++ lib/newpoc-action-user-edit/lib/newpoc/action/user/edit.rb
@@ -1,4 +1,6 @@
 
+require 'wisper'
+
 require 'newpoc/action/user/edit/version'
 
 module Newpoc
@@ -6,7 +8,41 @@ module Newpoc
     module User
       # Edit-user domain-logic setup verifies that a user is logged in.
       class Edit
-        # Your code goes here...
+        include Wisper::Publisher
+
+        def initialize(current_user, user_repository, options = {})
+          @current_user = current_user
+          @user_repository = user_repository
+          @success_event = options.fetch :success, :success
+          @failure_event = options.fetch :failure, :failure
+        end
+
+        def execute
+          find_user_for_slug
+          broadcast_success @entity
+        rescue RuntimeError
+          broadcast_failure current_user.slug
+        end
+
+        private
+
+        attr_reader :current_user, :entity, :failure_event, :success_event,
+                    :user_repository
+
+        def broadcast_success(payload)
+          broadcast success_event, payload
+        end
+
+        def broadcast_failure(payload)
+          broadcast failure_event, payload
+        end
+
+        def find_user_for_slug
+          result = user_repository.find_by_slug current_user.slug
+          @entity = result.entity
+          return if result.success?
+          fail current_user.slug.to_s
+        end
       end
     end
   end
--- lib/newpoc-action-user-edit/spec/newpoc/action/user/edit_spec.rb
+++ lib/newpoc-action-user-edit/spec/newpoc/action/user/edit_spec.rb
@@ -1,11 +1,119 @@
+
 require 'spec_helper'
 
+require 'fancy-open-struct'
+require 'wisper_subscription'
+
 describe Newpoc::Action::User::Edit do
+  let(:user_repo) do
+    Class.new do
+      def initialize(returned_result)
+        @returned_result = returned_result
+      end
+
+      def find_by_slug(slug)
+        @returned_result if slug # should always be true; silences RuboCop :P
+      end
+    end.new(result)
+  end
+  let(:current_user) { FancyOpenStruct.new slug: target_slug }
+  let(:result) do
+    FancyOpenStruct.new entity: the_entity, :success? => result_success
+  end
+  let(:subscriber) { WisperSubscription.new }
+  let(:success_entity) { FancyOpenStruct.new slug: target_slug }
+  let(:target_slug) { 'target_slug' }
+
   it 'has a version number' do
     expect(Newpoc::Action::User::Edit::VERSION).not_to be nil
   end
 
-  xit 'does something useful' do
-    expect(false).to eq(true)
-  end
+  context 'with default option settings' do
+    let(:command) do
+      described_class.new current_user, user_repo
+    end
+
+    before :each do
+      subscriber.define_message :success
+      subscriber.define_message :failure
+      command.subscribe subscriber
+      command.execute
+    end
+
+    context 'when the repository query is successful' do
+      let(:result_success) { true }
+      let(:the_entity) { success_entity }
+
+      it 'broadcasts :success' do
+        expect(subscriber).to be_success
+      end
+
+      describe 'broadcasts :success with a payload which' do
+        let(:payload) { subscriber.payload_for(:success).first }
+
+        it 'is the expected entity' do
+          expect(payload).to eq success_entity
+        end
+      end # describe 'broadcasts :success with a payload which'
+    end # context 'when the repository query is successful'
+
+    context 'when the repository query is unsuccessful' do
+      let(:result_success) { false }
+      let(:the_entity) { nil }
+
+      it 'broadcasts :failure' do
+        expect(subscriber).to be_failure
+      end
+
+      describe 'broadcasts :failure with a payload which' do
+        let(:payload) { subscriber.payload_for(:failure).first }
+
+        it 'is the original target slug' do
+          expect(payload).to eq target_slug
+        end
+      end # describe 'broadcasts :success with a payload which'
+    end # context 'when the repository query is unsuccessful'
+  end # context 'with default option settings'
+
+  context 'with non-default option settings' do
+    let(:command) do
+      described_class.new current_user, user_repo, options
+    end
+    let(:options) { { success: :message_one, failure: :message_two } }
+
+    before :each do
+      subscriber.define_message :message_one
+      subscriber.define_message :message_two
+      subscriber.define_message :success
+      subscriber.define_message :failure
+      command.subscribe subscriber
+      command.execute
+    end
+
+    context 'when the repository query is successful' do
+      let(:result_success) { true }
+      let(:the_entity) { success_entity }
+
+      it 'broadcasts the specified event for success' do
+        expect(subscriber).to be_message_one
+      end
+
+      it 'does not broadcast the default :success event' do
+        expect(subscriber).not_to be_success
+      end
+    end # context 'when the repository query is successful'
+
+    context 'when the repository query is unsuccessful' do
+      let(:result_success) { false }
+      let(:the_entity) { nil }
+
+      it 'broadcasts the specified event for failure' do
+        expect(subscriber).to be_message_two
+      end
+
+      it 'does not broadcast the default :failure event' do
+        expect(subscriber).not_to be_failure
+      end
+    end # context 'when the repository query is unsuccessful'
+  end # context 'with non-default option settings'
 end
