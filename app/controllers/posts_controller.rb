1a4093aa78baf9e27c665cff9e0030d045b63d69
Jeff Dickey <jdickey@seven-sigma.com>
2015-02-27 00:42:43 +0800

    Moved PostsController Create action to own source.
    Just shuffling the deck chairs this time.
    
    RSpec: 530+25+11+9+9+18+5+2+5+11+5+5+9+39+24+30 (737) examples, 0
    failures
    RuboCop: 243 files inspected, no offenses found

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,11 +4,11 @@ require 'newpoc/action/post/new'
 require 'newpoc/action/post/show'
 require 'newpoc/action/post/update'
 
-require 'create_post'
+# require 'create_post'
 
 require_relative 'posts_controller/error_message_builder'
 
-require_relative 'posts_controller/action/create/internals/guest_user_access'
+require_relative 'posts_controller/action/create'
 
 # PostsController: actions related to Posts within our "fancy" blog.
 class PostsController < ApplicationController
@@ -18,111 +18,8 @@ class PostsController < ApplicationController
   private_constant :Internals
   include Internals
 
-  # Isolating our Action classes within the controller they're associate with.
+  # Isolating our Action classes within the controller they're associated with.
   module Action
-    # Wisper-based command object called by Posts controller #create action.
-    class Create
-      module Internals
-        # Filters incoming post_data parameter and makes an OpenStruct of it.
-        class PostDataFilter
-          attr_reader :draft_post
-
-          def initialize(post_data)
-            @data = hash_input_data(post_data)
-            @draft_post = false
-          end
-
-          def filter
-            attribs = copy_attributes
-            @draft_post = true if data_defines_draft?
-            OpenStruct.new attribs.to_h.select { |_k, v| v }
-          end
-
-          private
-
-          attr_reader :data
-
-          def copy_attributes
-            ret = Struct.new(*post_attributes).new
-            post_attributes.each do |attrib|
-              ret[attrib] = data[attrib].to_s.strip if data[attrib].present?
-            end
-            ret
-          end
-
-          def data_defines_draft?
-            data[:post_status] == 'draft'
-          end
-
-          def hash_input_data(data)
-            data.send(hasher_for(data)).symbolize_keys
-          end
-
-          def hasher_for(data)
-            return :to_unsafe_h if data.respond_to? :to_unsafe_h
-            :to_h
-          end
-
-          def post_attributes
-            %w(author_name title body image_url slug created_at updated_at
-               pubdate post_status).map(&:to_sym)
-          end
-        end # class Actions::Create::Internals::PostDataFilter
-      end
-      private_constant :Internals
-      include Internals
-      include Wisper::Publisher
-
-      def initialize(current_user:, post_data:)
-        filter = PostDataFilter.new(post_data)
-        @post_data = filter.filter
-        @draft_post = filter.draft_post
-        @current_user = current_user
-      end
-
-      def execute
-        prohibit_guest_access
-        validate_post_data
-        add_entity_to_repository
-        broadcast_success @entity
-      rescue RuntimeError => message_or_bad_entity
-        broadcast_failure message_or_bad_entity
-      end
-
-      private
-
-      attr_reader :current_user, :draft_post, :entity, :post_data
-
-      def broadcast_failure(payload)
-        broadcast :failure, payload
-      end
-
-      def broadcast_success(payload)
-        broadcast :success, payload
-      end
-
-      def add_entity_to_repository
-        result = PostRepository.new.add entity
-        fail entity.to_json unless result.success?
-        # DON'T just use the existing entity; it (shouldn't) have its slug set,
-        # whereas the one that's been persisted and passed back through the
-        # `StoreResult` does. (That's how `Entity::Post` determines whether it's
-        # been persisted or not: whether the `slug` attribute is set.)
-        @entity = result.entity
-      end
-
-      def prohibit_guest_access
-        GuestUserAccess.new(current_user).prohibit
-      end
-
-      def validate_post_data
-        attribs = post_data.to_h.symbolize_keys
-        attribs[:author_name] ||= current_user.name
-        @entity = Newpoc::Entity::Post.new attribs
-        return if @entity.valid?
-        fail @entity.to_json
-      end
-    end # class PostsController::Action::Create
   end
 
   def index
--- spec/controllers/posts_controller/action/create_spec.rb
+++ spec/controllers/posts_controller/action/create_spec.rb
@@ -3,6 +3,44 @@ require 'spec_helper'
 require 'current_user_identity'
 require 'support/broadcast_success_tester'
 
+shared_examples 'a successful post that' do
+  it 'is successful' do
+    expect(subscriber).to be_successful
+    expect(subscriber).not_to be_failure
+  end
+
+  describe 'is successful, broadcasting a Newpoc::Entity::Post payload' do
+    let(:payload) { subscriber.payload_for(:success).first }
+
+    it 'which is valid' do
+      expect(payload).to be_valid
+    end
+
+    it 'with the correct field values set' do
+      acceptable_keys = [
+        :body,
+        :created_at,
+        :image_url,
+        :slug,
+        :title,
+        :updated_at
+      ]
+      expected = post_data
+                 .select { |k, _v| acceptable_keys.include? k }
+                 .merge author_name: current_user.name
+
+      expect(payload).to be_a Newpoc::Entity::Post
+      expected.each do |attrib, value|
+        expect(payload.attributes[attrib]).to eq value
+      end
+    end
+  end # describe 'is successful, broadcasting a Newpoc::Entity::Post payload'
+end # shared_examples 'a successful post that'
+
+# ############################################################################ #
+# ############################################################################ #
+# ############################################################################ #
+
 describe PostsController::Action::Create do
   let(:repo) { UserRepository.new }
   # NOTE: Old `Actions` namespace currently used here. Oops.
@@ -59,37 +97,102 @@ describe PostsController::Action::Create do
         }
       end
 
-      # it_behaves_like 'a successful post'
-      it 'is successful' do
-        expect(subscriber).to be_successful
-        expect(subscriber).not_to be_failure
+      it_behaves_like 'a successful post that'
+    end # context 'with minimal valid post data'
+
+    context 'with additional valid post data' do
+      let(:post_data) do
+        {
+          author_name: current_user.name,
+          title: 'A Title',
+          body: 'A Body',
+          image_url: 'http://example.com/image1.png'
+        }
       end
 
-      describe 'is successful, broadcasting a Newpoc::Entity::Post payload' do
-        let(:payload) { subscriber.payload_for(:success).first }
+      it_behaves_like 'a successful post that'
+    end # context 'with additional valid post data'
 
-        it 'which is valid' do
-          expect(payload).to be_valid
+    context 'with additional but invalid post data' do
+      let(:valid_data) do
+        {
+          author_name: current_user.name,
+          title: 'A Title',
+          body: 'A Body',
+          image_url: 'http://example.com/image1.png'
+        }
+      end
+      let(:invalid_data) { { bogus: 'an invalid post data attribute' } }
+      let(:post_data) { valid_data.merge invalid_data }
+
+      it_behaves_like 'a successful post that'
+
+      it 'does not include any invalid initialiser settings' do
+        payload = subscriber.payload_for(:success).first
+        invalid_data.each_key do |k|
+          expect(payload.attributes).not_to include k
+        end
+      end
+    end # context 'with additional but invalid post data'
+
+    context 'with post status set to' do
+      let(:actual) { subscriber.payload_for(:success).first }
+
+      context 'draft' do
+        let(:post_data) do
+          {
+            author_name: current_user.name,
+            title: 'A Title',
+            body: 'A Body',
+            post_status: 'draft'
+          }
         end
 
-        it 'with the correct field values set' do
-          acceptable_keys = [
-            :body,
-            :created_at,
-            :image_url,
-            :slug,
-            :title,
-            :updated_at
-          ]
-          expected = post_data.select { |k, _v| acceptable_keys.include? k }
-                     .merge author_name: current_user.name
+        it_behaves_like 'a successful post that'
 
-          expect(payload).to be_a Newpoc::Entity::Post
-          expected.each do |attrib, value|
-            expect(payload.attributes[attrib]).to eq value
-          end
+        it 'is a draft post' do
+          expect(actual).to be_draft
         end
-      end # describe 'is successful, broadcasting a ... payload'
-    end # context 'with minimal valid post data'
+      end # context 'draft'
+
+      context 'public' do
+        let(:post_data) do
+          {
+            author_name: current_user.name,
+            title: 'A Title',
+            body: 'A Body',
+            post_status: 'public'
+          }
+        end
+
+        it_behaves_like 'a successful post that'
+
+        it 'is not a draft post' do
+          expect(actual).not_to be_draft
+        end
+      end # context 'public'
+    end # context 'with post status set to'
+
+    context 'with insufficient valid post data' do
+      let(:post_data) { { body: 'A Body' } }
+
+      it 'is unsuccessful' do
+        expect(subscriber).not_to be_successful
+        expect(subscriber).to be_failure
+      end
+
+      describe 'is unsuccessful, broadcasting a payload with' do
+        let(:payload) { subscriber.payload_for(:failure).first }
+
+        it 'the expected errors' do
+          attribs = JSON.parse(payload.message).symbolize_keys
+          entity = Newpoc::Entity::Post.new attribs
+          expect(entity).not_to be_valid
+          expect(entity).to have(1).error
+          message = entity.errors.full_messages.first
+          expect(message).to eq "Title can't be blank"
+        end
+      end # describe 'is unsuccessful, broadcasting a payload with'
+    end # context 'with insufficient valid post data'
   end # context 'with a Registered User as the current user'
 end # describe PostsController::Action::Create
