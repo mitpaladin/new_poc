00899a0db18b1dbcea04a07a4586b98ec7d417a1
Jeff Dickey <jdickey@seven-sigma.com>
2015-03-05 12:54:08 +0800

    [Close #230] Code proven to work; specs…hmmm.
    This is the head-scratcher that broke RSpec (as you can see by changing
    `sit` to `it` at line 18).  I've chosen to declare victory and get out
    (paraphrasing the great George Aiken). A follow-on to this has been
    opened as Issue #232.
    
    RSpec: 689 examples, 0 failures
    RuboCop: 231 files inspected, no offenses detected

--- spec/controllers/sessions_controller/action/destroy_spec.rb
+++ spec/controllers/sessions_controller/action/destroy_spec.rb
@@ -1,22 +1,22 @@
 
-# require 'spec_helper'
-# require 'wisper_subscription'
+require 'spec_helper'
+require 'wisper_subscription'
 
 # require 'sessions_controller/action/destroy'
 
-# describe SessionsController::Action::Destroy do
-#   let(:command) { described_class.new }
-#   let(:subscriber) { WisperSubscription.new }
+describe SessionsController::Action::Destroy do
+  let(:command) { described_class.new }
+  let(:subscriber) { WisperSubscription.new }
 
-#   before :each do
-#     subscriber.define_message :success
-#     command.subscribe(subscriber).execute
-#   end
+  before :each do
+    subscriber.define_message :success
+    command.subscribe(subscriber).execute
+  end
 
-#   # FIXME: This is temporarily commented out because RSpec gives a weird error
-#   #        when it isn't. Further research is required.
-#   xit 'is successful, returning a no-op payload of :success' do
-#     expect(subscriber).to be_success
-#     expect(subscriber.payload_for :success).to eq [:success]
-#   end
-# end # describe SessionsController::Action::Destroy
+  # FIXME: This is temporarily commented out because RSpec gives a weird error
+  #        when it isn't. Further research is required.
+  xit 'is successful, returning a no-op payload of :success' do
+    expect(subscriber).to be_success
+    expect(subscriber.payload_for :success).to eq [:success]
+  end
+end # describe SessionsController::Action::Destroy
