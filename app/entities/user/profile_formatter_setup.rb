b221e0cc6e2a8aecabbfa494d59430c7e21300d1
Jeff Dickey <jdickey@seven-sigma.com>
2015-03-18 00:48:16 +0800

    [skip ci] Asymptotically close now.
    Added inadvertently-omitted Markdown converter. The things you drop on
    the floor and kick under the sofa…

--- app/entities/user/profile_formatter_setup.rb
+++ app/entities/user/profile_formatter_setup.rb
@@ -1,4 +1,6 @@
 
+require_relative 'profile_formatter_setup/internals'
+
 # Namespace containing all application-defined entities.
 module Entity
   # The `User` class is the *core business-logic entity* modelling users in the
@@ -9,6 +11,12 @@ module Entity
     # Adds the `#formatted_profile` method and its supporting attribute to an
     # object instance (presumably of `Entity::User`).
     class ProfileFormatterSetup
+      # Internal code used exclusively by `ProfileFormatterSetup`.
+      module Internals
+      end # module Entity::User::ProfileFormatterSetup::Internals
+      private_constant :Internals
+      extend Internals
+
       # Class method that creates a new attribute on an entity, setting it from
       # a value read from the incoming attributes. It then defines a method that
       # uses that attribute (presumably a Proc or lambda) to produce a value
@@ -18,7 +26,7 @@ module Entity
       #                   is used to initialise a new attribute on the entity.
       def self.setup(entity:, attributes:)
         entity.instance_variable_set :@markdown_converter,
-                                     attributes[:markdown_converter]
+                                     get_markdown_converter(attributes)
         entity.define_singleton_method :formatted_profile do
           @markdown_converter.call profile
         end
