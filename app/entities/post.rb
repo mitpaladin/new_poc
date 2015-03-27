f9a5d21f64730ffe340ba3bd56a49ed8f69ce795
Jeff Dickey <jdickey@seven-sigma.com>
2015-03-28 01:25:07 +0800

    Post now has extensible attributes value object.
    RSpec: 733 examples, 0 failures, 1 pending
    RuboCop: 242 files inspected, no offenses detected

--- app/entities/post.rb
+++ app/entities/post.rb
@@ -13,15 +13,15 @@ module Entity
     extend Forwardable
 
     # Value object containing attribute definitions used by this class.
-    class Attributes < ValueObject::Base
+    class CoreAttributes < ValueObject::Base
       has_fields :author_name, :body, :created_at, :image_url, :pubdate, :slug,
                  :title, :updated_at
     end
-    private_constant :Attributes
+    private_constant :CoreAttributes
 
-    attr_reader :attributes
-    def_delegators :@attributes, :author_name, :body, :created_at, :image_url,
-                   :pubdate, :slug, :title, :updated_at
+    # attr_reader :attributes
+    # def_delegators :@attributes, :author_name, :body, :created_at, :image_url,
+    #                :pubdate, :slug, :title, :updated_at
 
     def extend_with_presentation
       extend Extensions::Presentation
@@ -32,21 +32,54 @@ module Entity
     end
 
     def initialize(attributes)
-      @attributes = Attributes.new attributes
+      @attributes = {}
+      @attributes[:core] = CoreAttributes.new attributes
     end
 
     def instance_variable_set(_symbol, _obj)
       fail "Can't touch this."
     end
 
+    def method_missing(method_sym, *arguments, &block)
+      attribs = attributes
+      return super unless attribs.respond_to?(method_sym)
+      # We're asking for an attribute; let's define a reader for it so we
+      # don't come here again looking for that attribute
+      define_singleton_method method_sym do
+        attributes.send method_sym
+      end
+      send method_sym
+    end
+
+    def respond_to?(method, include_private = false)
+      super || attributes.fields.include?(method)
+    end
+
+    def attributes
+      all_attributes = collect_attributes
+      # OK; we've a *Hash* of every attribute in the entity; but what we really
+      # want is a *ValueObject*.
+      attributes_class(all_attributes.keys).new all_attributes
+    end
+
     def persisted?
       slug.present?
     end
 
-    # To implement an attribute setter, you'd do something like this. Note that
-    # we *replace* the `@attributes` object with a new value object.
-    # def author_name=(value)
-    #   @attributes = attributes.copy_with(:author_name, value)
-    # end
+    private
+
+    def attributes_class(attribute_keys)
+      Class.new(ValueObject::Base).tap do |ret|
+        attribute_keys.each { |key| ret.instance_eval { has_fields key } }
+      end
+    end
+
+    def collect_attributes
+      {}.tap do |all_attributes|
+        @attributes.each_key do |attributes_set|
+          all_attributes.merge! @attributes[attributes_set].to_hash
+        end
+      end
+    end
   end # class Entity::Post
 end
