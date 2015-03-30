40a2b802076fe3f58309476f7efc24ea982c6227
Jeff Dickey <jdickey@seven-sigma.com>
2015-03-30 15:37:07 +0800

    Almost done; extension interface cleaned up.
    Persistence extension now uses methods on the extending class, rather
    than reaching in and grabbing instance variables directly. This led to
    a nice rephrasing of `Post#load_extensions` and gets us nicely set up
    for yet another, final attempt at extracting attribute management into
    its own module.
    
    RSpec: 733 examples, 0 failures, 1 pending
    RuboCop: 243 files inspected, no offenses detected

--- app/entities/post.rb
+++ app/entities/post.rb
@@ -12,14 +12,29 @@ module Entity
   # It delegates or defers implementation-specific details such as persistence,
   # user interface, etc.
   class Post
-    extend Forwardable
-
     # Value object containing attribute definitions used by this class.
     class CoreAttributes < ValueObject::Base
       has_fields :author_name, :body, :image_url, :pubdate, :title
     end
     private_constant :CoreAttributes
 
+    ############################################################################
+    # Extensions
+    #
+    # Extensions come in two flavours, which I'm presently calling "functional"
+    # and "optional". Functional extensions, such as Presentation or Validation,
+    # add features (functionality) to the core entity. Optional extensions
+    # provide support for attributes not part of the core attribute set, and are
+    # added as a result of their attributes being specified as parameaters to
+    # `#initialize` (see the `AttributeExtensionMapper` class and the
+    # `#load_extensions` method of this class). Outside code working with entity
+    # instances should and must be kept blissfully unaware of the internal
+    # machinations going on; from its viewpoint, an attribute is an attribute.
+    #
+    # Continuing, functional extensions have corresponding methods on this class
+    # which extend an instance of this class with the extension in question;
+    # optional classes never do. (To load an optional extension, specify its
+    # attributes when instantiating this class.)
     def extend_with_presentation
       extend Extensions::Presentation
     end
@@ -29,13 +44,13 @@ module Entity
     end
 
     def initialize(attributes)
-      @attributes = {}
-      @attributes[:core] = CoreAttributes.new attributes
+      @original_attributes = attributes
+      @attributes_sets = {}
+      add_attributes_set :core, CoreAttributes.new(attributes)
       @extension_mapper = AttributeExtensionMapper.new.build do |mapping|
         mapping[:core] = CoreAttributes.fields
       end
-      @attributes_in = attributes
-      load_extensions attributes
+      load_extensions
     end
 
     def instance_variable_set(_symbol, _obj)
@@ -47,7 +62,7 @@ module Entity
       # We're asking for an attribute; let's define a reader for it so we
       # don't come here again looking for that attribute
       attr_set_index = ext_index_symbol_for method_sym
-      attribute_set = @attributes[attr_set_index]
+      attribute_set = @attributes_sets[attr_set_index]
       define_singleton_method method_sym do
         # Extension not loaded because triggering attributes not set. Bail.
         return nil unless attribute_set.respond_to? method_sym
@@ -68,11 +83,21 @@ module Entity
       attributes_class(all_attributes.keys).new all_attributes
     end
 
+    # This will be overridden by the Persistence extension if a `:slug`
+    # attribute is specified to the initialiser. Of course, since persisting an
+    # entity is supposed to set the `slug` *field*, if the attribute is missing
+    # the entity does not represent a persisted post. QED.
     def persisted?
-      slug.present?
+      false
     end
 
     private
+
+    attr_reader :original_attributes
+
+    def add_attributes_set(set_index, values)
+      @attributes_sets[set_index] = values
+    end
 
     def attributes_class(attribute_keys)
       Class.new(ValueObject::Base).tap do |ret|
@@ -82,8 +107,8 @@ module Entity
 
     def collect_attributes
       {}.tap do |all_attributes|
-        @attributes.each_key do |attributes_set|
-          all_attributes.merge! @attributes[attributes_set].to_hash
+        @attributes_sets.each_key do |attributes_set|
+          all_attributes.merge! @attributes_sets[attributes_set].to_hash
         end
       end
     end
@@ -92,8 +117,8 @@ module Entity
       @extension_mapper[method_sym].to_s.split('::').last.downcase.to_sym
     end
 
-    def load_extensions(attributes)
-      attributes.each_key do |k|
+    def load_extensions
+      original_attributes.each_key do |k|
         extension = @extension_mapper[k]
         next if [nil, :core].include?(extension)
         extend extension
--- app/entities/post/extensions/persistence.rb
+++ app/entities/post/extensions/persistence.rb
@@ -6,11 +6,20 @@ module Entity
   # user interface, etc.
   class Post
     # Extensions to Post entity beyond core attribute manipulation.
+    #
+    # Unlike extensions such as Validation or Presentation, this module is not
+    # (ordinarily) explicitly loaded by a `Post` method needing to perform an
+    # action, but in response to attributes being specified to the `#initialize`
+    # method of `Post`. See `Post#initialize` and `Post#method_missing` to get a
+    # sense of how this works.
+    #
+    # IMPORTANT NOTE: This extension, like all optional extensions, uses the
+    # `original_attributes` instance method on the extending class to get the
+    # attribute values specified *to* that class' `#initialize` method. It also
+    # calls that class' `#add_attributes_set` instance method.
     module Extensions
       # Persistence-state attribute readers pertinent to Post entities.
       module Persistence
-        extend Forwardable
-
         # Value object containing attribute definitions used by this class.
         class PersistenceAttributes < ValueObject::Base
           has_fields :created_at, :slug, :updated_at
@@ -19,7 +28,8 @@ module Entity
 
         def self.extended(base)
           base.instance_eval do
-            @attributes[:persistence] = PersistenceAttributes.new @attributes_in
+            new_values = PersistenceAttributes.new original_attributes
+            add_attributes_set :persistence, new_values
           end
         end
 
