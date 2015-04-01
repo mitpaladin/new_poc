ce9b82ecb2e5bdea92db2aec68c23aba299edc73
Jeff Dickey <jdickey@seven-sigma.com>
2015-04-01 23:59:48 +0800

    PostEntityFactory create using attribute hash.

--- app/entity_factories/post_factory.rb
+++ app/entity_factories/post_factory.rb
@@ -3,9 +3,18 @@ require 'post'
 
 # Class to create (and setup if needed) instance of DM entity for use cases.
 class PostFactory
+  module Internals
+    def as_hash(record)
+      h = record.respond_to?(:attributes) ? record.attributes : record
+      h.to_hash.symbolize_keys
+    end
+  end # module PostFactory::Internals
+  private_constant :Internals
+  extend Internals
+
   class << self
     def create(record)
-      entity_class.new record.attributes.symbolize_keys
+      entity_class.new as_hash(record)
     end
 
     def entity_class
--- spec/entity_factories/shared_examples/an_entity_factory_for.rb
+++ spec/entity_factories/shared_examples/an_entity_factory_for.rb
@@ -1,11 +1,22 @@
 
 shared_examples 'an entity factory for' do |entity_class|
-  describe ".create returns a #{entity_class}" do
+  describe ".create returns a #{entity_class} instance" do
     let(:entity) { described_class.create dao }
 
-    it 'instance' do
-      expect(entity).to be_an entity_class
-    end
+    describe 'when called with' do
+      after :each do
+        expect(@entity).to be_an entity_class
+      end
+
+      it 'an object with appropriate attribute methods' do
+        @entity = entity
+      end
+
+      it 'a Hash with appropriate attribute key/value pairs' do
+        attributes = dao.attributes.to_hash.symbolize_keys
+        @entity = described_class.create attributes
+      end
+    end # describe 'when called with'
 
     it 'with the same attribute values as the DAO' do
       post_keys = %w(author_name body created_at image_url pubdate slug title
@@ -20,7 +31,7 @@ shared_examples 'an entity factory for' do |entity_class|
         expect(entity.send attrib).to eq dao[attrib]
       end
     end
-  end # describe ".create returns a #{entity_class}"
+  end # describe ".create returns a #{entity_class} instance"
 
   it ".entity_class returns the #{entity_class} class" do
     expect(described_class).to respond_to :entity_class
