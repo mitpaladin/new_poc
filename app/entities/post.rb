4593eb3dceb4f4a7fc70181be470563c576932f1
Jeff Dickey <jdickey@seven-sigma.com>
2015-04-08 17:50:33 +0800

    Initial integration of AttributeContainer for Post
    Painless enough, if a bit ceremonial (see lines 26-28).
    
    RSpec: 722 examples, 0 failures
    RuboCop: 234 files inspected, 0 offenses detected

--- app/entities/post.rb
+++ app/entities/post.rb
@@ -1,15 +1,31 @@
 
+require_relative 'post/attribute_container'
+
 # Namespace containing all application-defined entities.
 module Entity
   # The Post class encapsulates post-related domain logic of our "fancy" blog.
   # It delegates or defers implementation-specific details such as persistence,
   # user interface, etc.
   class Post
-    attr_reader :author_name, :title
+    def initialize(attributes_in)
+      init_attributes attributes_in
+      define_attribute_readers
+    end
 
-    def initialize(attributes)
-      @title = attributes[:title].to_s
-      @author_name = attributes[:author_name].to_s
+    private
+
+    def define_attribute_readers
+      @attributes.attributes.to_hash.each_key do |key|
+        self.class.send :define_method, key do
+          @attributes.attributes.send key
+        end
+      end
+    end
+
+    def init_attributes(attributes_in)
+      attrs = AttributeContainer.new attributes_in
+      whitelist = [:author_name, :title]
+      @attributes = AttributeContainer.whitelist_from attrs, *whitelist
     end
   end # class Entity::Post
 end
