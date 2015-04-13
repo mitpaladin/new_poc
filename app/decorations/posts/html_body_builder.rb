2f1940ceb2103ae9f1b75e68303765a14b2d4fe3
Jeff Dickey <jdickey@seven-sigma.com>
2015-04-13 15:46:04 +0800

    New Decorations::Posts::HtmlBodyBuilder class.
    When complete, decouples this bit of presentation logic from direct
    inclusion in the `Post` entity class itself. The latter will have an
    extension that essentially forwards what a view thinks of as a call to
    `Post#build_body` to `HtmlBodyBuilder#build`, passing in the `Post`
    entity in question. This is code I should have written lo, these many
    moons ago.
    
    RSpec: 806 examples, 0 failures, 1 pending
    RuboCop: 257 files inspected, no offenses detected

---
+++ app/decorations/posts/html_body_builder.rb
@@ -0,0 +1,59 @@
+
+require 'newpoc/services/markdown_html_converter'
+
+# POROs that act as presentational support for entities.
+module Decorations
+  # Decorations for `Post` entities. D'oh!
+  module Posts
+    # The `HtmlBodyBuilder` `Post` decoration builds an HTML representation of
+    # the body of the `post` instance passed into the `#build` method. That
+    # representation will be either
+    #
+    # 1. the post body as HTML, if there is no `image_url` on the `Post`; or
+    # 2. the post body and image URL within a `figure`; see #build for details.
+    #
+    class HtmlBodyBuilder
+      # Overriding the default `markdown_converter` parameter is mostly
+      # useful for situations where the app makes use of Pivotal's "unbuilt
+      # Rails dependency" idiom. As we're no longer doing that, we *could*
+      # just rip it out entirely. One step at a time.
+      def initialize(markdown_converter = default_markdown_converter)
+        @markdown_converter = markdown_converter
+        return if markdown_converter.respond_to? :to_html
+        fail ArgumentError, 'parameter must respond to the :to_html message'
+      end
+
+      # Builds an HTML representation of the post contents, in a format that
+      # depends on whether the `Post`'s `image_url` attribute is present.
+      #
+      # With no image URL, converts the post body (which may contain any valid
+      # combination of text and Markdown formatting (which allows raw HTML)) to
+      # HTML, and returns that.
+      #
+      # *With* an image URL, generates an HTML `figure` tag pair containing the
+      # image URL wrapped in an `img` tag pair, and a `figcaption` tag pair
+      # containing the (converted) post body, returning the generated `figure`
+      # fragment.
+      def build(post)
+        @post = post
+        return markdown_converter.to_html(post.body) if text_post?
+        build_image_post
+      end
+
+      private
+
+      attr_reader :markdown_converter, :post
+
+      def build_image_post
+      end
+
+      def default_markdown_converter
+        Newpoc::Services::MarkdownHtmlConverter.new
+      end
+
+      def text_post?
+        post.image_url.present? == false
+      end
+    end # class Decorations::Posts::HtmpBodyBuilder
+  end
+end
---
+++ spec/decorations/posts/html_body_builder_spec.rb
@@ -0,0 +1,54 @@
+
+require 'spec_helper'
+
+require 'posts/html_body_builder'
+
+# POROs that act as presentational support for entities.
+module Decorations
+  describe Posts::HtmlBodyBuilder do
+    describe 'can be initialised' do
+      it 'without parameters' do
+        expect { described_class.new }.not_to raise_error
+      end
+
+      it 'with a MarkdownHtmlConverter instance' do
+        converter = Newpoc::Services::MarkdownHtmlConverter.new
+        expect { described_class.new converter }.not_to raise_error
+      end
+    end # describe 'can be initialised'
+
+    it 'cannot be initialised with an invalid parameter' do
+      expected = 'parameter must respond to the :to_html message'
+      expect { described_class.new 'bogus' }.to raise_error ArgumentError,
+                                                            expected
+    end
+
+    describe 'has a #build method that when called with a Post' do
+      let(:obj) { described_class.new }
+
+      context 'not containing an image URL' do
+        let(:post) { FancyOpenStruct.new title: 'Post Title' }
+
+        it 'wraps a simple string body in an HTML paragraph tag pair' do
+          post.body = 'Simple Text'
+          expected = ['<p>', '</p>'].join post.body
+          expect(obj.build(post)).to eq expected
+        end
+
+        it 'converts a body containing Markdown to HTML' do
+          post.body = "This *is* a `test`.\n\n1. Foo;\n1. Bar.\n\nAll done."
+          expected = "<p>This <em>is</em> a <code>test</code>.</p>\n\n" \
+            "<ol>\n<li>Foo;</li>\n<li>Bar.</li>\n</ol><p>All done.</p>"
+          expect(obj.build(post)).to eq expected
+        end
+      end # context 'not containing an image URL'
+
+      context 'containing an image URL' do
+        let(:post) do
+          url = 'http://www.example.com/image1.png'
+          FancyOpenStruct.new title: 'Post Title', image_url: url
+        end
+      end # context 'containing an image URL'
+    end # describe 'has a #build method that when called with a Post'
+  end # describe Posts::HtmlBodyBuilder
+end
