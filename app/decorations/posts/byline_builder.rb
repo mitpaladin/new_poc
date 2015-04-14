7c141d097b2fb2909e9b89a7af4a0d7ff35630ef
Jeff Dickey <jdickey@seven-sigma.com>
2015-04-15 02:03:13 +0800

    Extracted BylineBuilder attribute builder.
    RSpec: 831 examples, 0 failures, 1 pending
    RuboCop: 264 files inspected, no offenses detected

--- app/decorations/posts/byline_builder.rb
+++ app/decorations/posts/byline_builder.rb
@@ -1,6 +1,8 @@
 
 require 'timestamp_builder'
 
+require_relative 'byline_builder/attributes'
+
 # POROs that act as presentational support for entities.
 module Decorations
   # Decorations for `Post` entities. D'oh!
@@ -8,28 +10,17 @@ module Decorations
     # Builds an HTML fragment (a paragraph) containing a timestamp and author-
     # name attribution.
     class BylineBuilder
+      extend Forwardable
       include TimestampBuilder
 
       def build(post)
-        init_attributes post
+        @attributes = Attributes.new(post)
         build_element.to_html
       end
 
       private
 
-      attr_reader :author_name, :pubdate, :updated_at
-
-      def attributes_for(post)
-        if post.respond_to? :to_hash
-          post.to_hash.symbolize_keys
-        elsif post.respond_to? :attributes
-          post.attributes.to_hash.symbolize_keys
-        else
-          message = 'Post must expose its attributes either through an' \
-            ' #attributes or #to_hash method'
-          fail message
-        end
-      end
+      def_delegators :@attributes, :author_name, :pubdate, :updated_at
 
       def build_element
         doc = Nokogiri::HTML::Document.new
@@ -41,19 +32,6 @@ module Decorations
 
       def draft?
         pubdate.nil?
-      end
-
-      def enforce_author_name
-        message = 'post must have an :author_name attribute value'
-        fail message unless @author_name
-      end
-
-      def init_attributes(post)
-        attributes = attributes_for post
-        @pubdate = attributes[:pubdate]
-        @author_name = attributes[:author_name]
-        @updated_at = attributes[:updated_at] || Time.now.utc
-        enforce_author_name
       end
 
       def inner(doc)
