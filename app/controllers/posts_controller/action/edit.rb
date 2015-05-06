48709132eb037f99cd85b1f6bc33bbc6933c7cf8
Jeff Dickey <jdickey@seven-sigma.com>
2015-05-06 14:09:13 +0800

    Contracts added to Action::Edit.
    Also broke `NotAuthorFailure` out of superfluous `Internals` namespace.
    
    RSpec: 872 examples, 0 failures, 1 pending
    RuboCop: 277 files inspected, no offenses detected
    Coverage: 2394 / 2451 LOC (97.67%) covered.

--- app/controllers/posts_controller/action/edit.rb
+++ app/controllers/posts_controller/action/edit.rb
@@ -1,4 +1,7 @@
 
+require 'contracts'
+
+require 'post'
 require 'action_support/broadcaster'
 require 'action_support/guest_user_access'
 require 'action_support/slug_finder'
@@ -9,58 +12,74 @@ class PostsController < ApplicationController
   module Action
     # Verifies that the current user is permitted to edit a specified post.
     class Edit
-      module Internals
-        # Current user not author of specified post. Bitch about it, as JSON.
-        class NotAuthorFailure
-          def initialize(user_name:, post:)
-            @user_name = user_name
-            @post = post
-          end
+      # Current user not author of specified post. Bitch about it, as JSON.
+      class NotAuthorFailure
+        def initialize(user_name:, post:)
+          @user_name = user_name
+          @post = post
+        end
 
-          def to_yaml
-            YAML.dump(
-              current_user_name: @user_name,
-              author_name:       @post.author_name,
-              post_slug:         @post.slug.to_s
-            )
-          end
-        end # class Newpoc::Action::Post::Edit::Internals::NotAuthorFailure
-      end
-      private_constant :Internals
-      include Internals
+        def to_yaml
+          YAML.dump(
+            current_user_name: @user_name,
+            author_name:       @post.author_name,
+            post_slug:         @post.slug.to_s
+          )
+        end
+      end # class Newpoc::Action::Post::Edit::NotAuthorFailure
+      private_constant :NotAuthorFailure
 
       include ActionSupport::Broadcaster
+      include Contracts
 
+      INIT_CONTRACT_INPUTS = {
+        slug: String,
+        # Currently called with the class built in the spec, a DAO, or entity.
+        current_user: RespondTo[:name],
+        repository: RespondTo[:find_by_slug]
+      }
+
+      Contract INIT_CONTRACT_INPUTS => Edit
       def initialize(slug:, current_user:, repository:)
+        ap [:line_43, current_user.class] unless current_user.is_a? UserDao
         @slug = slug
         @current_user = current_user
         @repository = repository
+        self
       end
 
+      Contract None => Edit
       def execute
         prohibit_guest_access
         validate_slug
         verify_user_is_author
         broadcast_success entity
+        self
       rescue RuntimeError => e
         broadcast_failure e.message
+        self
       end
 
       private
 
       attr_reader :current_user, :entity, :repository, :slug
 
+      Contract None => Edit
       def prohibit_guest_access
         ActionSupport::GuestUserAccess.new(current_user).prohibit
+        self
       end
 
+      Contract None => Edit
       def validate_slug
         @entity = ActionSupport::SlugFinder.new(slug: slug,
                                                 repository: repository)
                   .find
                   .entity
+        self
       end
 
+      Contract None => nil
       def verify_user_is_author
         return if current_user.name == entity.author_name
         fail NotAuthorFailure.new(user_name: current_user.name, post: entity)
