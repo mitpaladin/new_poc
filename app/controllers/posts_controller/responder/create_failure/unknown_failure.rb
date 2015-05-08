7396513c57b041040758f9b3fcf889522bfac2b1
Jeff Dickey <jdickey@seven-sigma.com>
2015-05-08 13:27:27 +0800

    Contracts added to CreateFailure::UnknownFailure.
    RSpec: 898 examples, 0 failures, 1 pending
    RuboCop: 277 files inspected, no offenses detected
    Coverage: 2454 / 2473 LOC (99.23%) covered.

--- app/controllers/posts_controller/responder/create_failure/unknown_failure.rb
+++ app/controllers/posts_controller/responder/create_failure/unknown_failure.rb
@@ -1,29 +1,31 @@
 
+require 'contracts'
+
 # PostsController: actions related to Posts within our "fancy" blog.
 class PostsController < ApplicationController
   # A Responder responds to the reported result of an application action in an
   # implementation-appropriate manner.
   module Responder
-    # Takes an error-message string as input and redirects to the root path,
-    # using the error message as an alert flash.
-    #
-    # Makes use of these methods on the controller passed into `#initialize`:
-    #
-    # 1. `:instance_variable_set`;
-    # 2. `:redirect_to`;
-    # 3. `:render`; and
-    # 4. `:root_path`.
-    #
+    # Takes a set of instance values for initialisation, and arbitrary data
+    # passed to the `#call` method, and raises an "Unknown failure" error,
+    # dumping the instance values and '#call' input data to the error message in
+    # a human-readable format.
     class CreateFailure
       class UnknownFailure
+        include Contracts
+
+        Contract RespondTo[:to_hash] => UnknownFailure
         def initialize(ivars = {})
           @ivars = ivars
+          self
         end
 
+        Contract Any => true
         def self.applies?(_payload)
           true
         end
 
+        Contract Any => Any
         def call(*args)
           message = "Unknown failure:\nargs: #{args.ai}\nivars: #{@ivars.ai}"
           fail message
