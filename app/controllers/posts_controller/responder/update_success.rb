f7379f627871b4c3ebeb276e41ed12580a85b894
Jeff Dickey <jdickey@seven-sigma.com>
2015-05-09 02:17:01 +0800

    Contracts added to `Responder::UpdateSuccess`.
    RSpec: 910 examples, 0 failures, 1 pending
    RuboCop: 279 files inspected, no offenses detected
    Coverage: 2538 / 2557 LOC (99.26%) covered.

--- app/controllers/posts_controller/responder/update_success.rb
+++ app/controllers/posts_controller/responder/update_success.rb
@@ -1,5 +1,5 @@
 
-require 'post_dao'
+require 'contracts'
 
 # PostsController: actions related to Posts within our "fancy" blog.
 class PostsController < ApplicationController
@@ -16,42 +16,57 @@ class PostsController < ApplicationController
     # 3. `:post_path`.
     #
     class UpdateSuccess
+      include Contracts
+
+      INIT_CONTRACT_INPUTS = RespondTo[:instance_variable_set, :post_path,
+                                       :redirect_to]
+
+      Contract INIT_CONTRACT_INPUTS => UpdateSuccess
       def initialize(controller)
         @redirect_to = controller.method :redirect_to
         @post_path = controller.method :post_path
         @post_setter = -> (dao) { controller.instance_variable_set :@post, dao }
+        self
       end
 
+      Contract RespondTo[:slug, :title] => UpdateSuccess
       def respond_to(payload)
         @post_title = payload.title
         assign_dao_ivar(payload)
         redirect_to_post
+        self
       end
 
       private
 
       attr_reader :post_path, :post_setter, :post_title, :redirect_to
 
+      Contract RespondTo[:slug] => PostDao
       def dao_from_entity(entity)
         repo.dao.find_by_slug(entity.slug).tap do |dao|
           dao.extend PostDao::Presentation
         end
       end
 
+      Contract None => Hash
       def flash_options
         message = "Post '#{post_title}' successfully updated."
         { flash: { success: message } }
       end
 
+      Contract None => UpdateSuccess
       def redirect_to_post
         redirect_to.call post_path.call, flash_options
+        self
       end
 
+      Contract RespondTo[:slug] => UpdateSuccess
       def assign_dao_ivar(entity)
         post_setter.call dao_from_entity(entity)
         self
       end
 
+      Contract None => PostRepository
       def repo
         @repo ||= PostRepository.new
       end
