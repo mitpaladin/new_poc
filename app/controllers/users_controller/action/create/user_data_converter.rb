a42500a27b37415c4d1a0933b2c38485c2bc7783
Jeff Dickey <jdickey@seven-sigma.com>
2015-05-09 19:16:31 +0800

    Contracts added to UserDataConverter.
    RSpec: 915 examples, 0 failures, 1 pending
    RuboCop: 281 files inspected, no offenses detected
    Coverage: 2553 / 2572 LOC (99.26%) covered.

--- app/controllers/users_controller/action/create/user_data_converter.rb
+++ app/controllers/users_controller/action/create/user_data_converter.rb
@@ -1,4 +1,6 @@
 
+require 'contracts'
+
 # UsersController: actions related to Users within our "fancy" blog.
 class UsersController < ApplicationController
   # Isolating our Action classes within the controller they're associate with.
@@ -9,10 +11,14 @@ class UsersController < ApplicationController
       # and then replace each array holding a value with that value.
       # This very well may be more general-purpose than just this action...
       class UserDataConverter
+        include Contracts
+
         attr_reader :data
 
+        Contract Or[String, RespondTo[:to_hash]] => UserDataConverter
         def initialize(input)
           @data = parse input
+          self
         end
 
         private
@@ -20,12 +26,13 @@ class UsersController < ApplicationController
         # CGI.parse will *always* return key/value pairs using *arrays* of
         # values, even when there is only one. This understandably FUBARs code
         # expecting simple key/value pairs.
+        Contract String => HashOf[Symbol, Any]
         def data_from(input)
           data = CGI.parse input
-          data.each { |k, v| data[k] = v.first }
-          data.symbolize_keys
+          data.each { |k, v| data[k] = v.first }.symbolize_keys
         end
 
+        Contract Or[String, RespondTo[:to_hash]] => FancyOpenStruct
         def parse(input)
           case input
           when String
