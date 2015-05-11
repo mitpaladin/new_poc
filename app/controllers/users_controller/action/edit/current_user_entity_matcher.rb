872f30e9c687fedd2e040d96f933ff200eabc46b
Jeff Dickey <jdickey@seven-sigma.com>
2015-05-11 13:25:12 +0800

    Added Contracts to CurrentUserEntityMatcher.
    RSpec: 940 examples, 0 failures, 1 pending
    RuboCop: 287 files inspected, no offenses detected
    Coverage: 2599 / 2618 LOC (99.27%) covered.

--- app/controllers/users_controller/action/edit/current_user_entity_matcher.rb
+++ app/controllers/users_controller/action/edit/current_user_entity_matcher.rb
@@ -1,4 +1,6 @@
 
+require 'contracts'
+
 # UsersController: actions related to Users within our "fancy" blog.
 class UsersController < ApplicationController
   module Action
@@ -7,11 +9,21 @@ class UsersController < ApplicationController
       # Match current user with entity alleged to be current user. Fail if
       # match fails.
       class CurrentUserEntityMatcher
+        include Contracts
+
+        INIT_CONTRACT_INPUTS = {
+          current_user: RespondTo[:name],
+          entity: RespondTo[:name]
+        }
+
+        Contract INIT_CONTRACT_INPUTS => CurrentUserEntityMatcher
         def initialize(current_user:, entity:)
           @current_user = current_user
           @entity = entity
+          self
         end
 
+        Contract None => CurrentUserEntityMatcher
         def match
           return self if current_user.name == entity.name
           fail_with_dump
@@ -21,6 +33,7 @@ class UsersController < ApplicationController
 
         attr_reader :current_user, :entity
 
+        Contract None => String
         def fail_with_dump
           error_data = {
             not_user: entity.name,
