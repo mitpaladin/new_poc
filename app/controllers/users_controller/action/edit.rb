cb6a2668c97f53e1a5f62ff4f7675ff4eda83aa6
Jeff Dickey <jdickey@seven-sigma.com>
2015-05-12 00:13:28 +0800

    Contacts added to `Action::Edit`.
    RSpec: 944 examples, 0 failures, 1 pending
    RuboCop: 287 files inspected, no offenses detected
    Coverage: 2638 / 2657 LOC (99.28%) covered.

--- app/controllers/users_controller/action/edit.rb
+++ app/controllers/users_controller/action/edit.rb
@@ -1,4 +1,6 @@
 
+require 'contracts'
+
 require 'action_support/slug_finder'
 
 require_relative 'edit/current_user_entity_matcher'
@@ -9,34 +11,48 @@ class UsersController < ApplicationController
     # Edit-user domain-logic setup verifies that a user is logged in.
     class Edit
       include ActionSupport::Broadcaster
+      include Contracts
 
       attr_reader :entity
 
+      INIT_CONTRACT_INPUTS = {
+        slug: String,
+        current_user: RespondTo[:name],
+        user_repository: RespondTo[:find_by_slug]
+      }
+
+      Contract INIT_CONTRACT_INPUTS => Edit
       def initialize(slug:, current_user:, user_repository:)
         @slug = slug
         @current_user = current_user
         @user_repository = user_repository
+        self
       end
 
+      Contract None => Edit
       def execute
         find_user_for_slug
         verify_current_user
         broadcast_success entity
+        self
       rescue RuntimeError => error
         error_obj = Yajl.load error.message, symbolize_keys: true
         broadcast_failure error_obj
+        self
       end
 
       private
 
       attr_reader :current_user, :slug, :user_repository
 
+      Contract None => Entity::User
       def find_user_for_slug
         @entity = ActionSupport::SlugFinder.new(slug: slug,
                                                 repository: user_repository)
                   .find.entity
       end
 
+      Contract None => Any # raises an error on mismatch; else don't care
       def verify_current_user
         CurrentUserEntityMatcher.new(current_user: current_user, entity: entity)
           .match
