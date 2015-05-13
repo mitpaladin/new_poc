5ff44ba76dbd3996cd576af3b9f3df758c5b264d
Jeff Dickey <jdickey@seven-sigma.com>
2015-05-13 12:39:15 +0800

    Added Contracts to `PostDao::Presentation` module.
    RSpec: 960 examples, 0 failures, 1 pending
    RuboCop: 289 files inspected, no offenses detected
    Coverage: 2653 / 2672 LOC (99.29%) covered.

--- app/daos/post_dao/presentation.rb
+++ app/daos/post_dao/presentation.rb
@@ -1,4 +1,6 @@
 
+require 'contracts'
+
 require 'posts/html_body_builder'
 require 'posts/byline_builder'
 
@@ -18,39 +20,49 @@ class PostDao < ActiveRecord::Base
   # `Gemfile.lock`.
   module Presentation
     include TimestampBuilder
+    include Contracts
 
+    Contract None => String
     def build_body
       Decorations::Posts::HtmlBodyBuilder.new.build self
     end
 
+    Contract None => String
     def build_byline
       Decorations::Posts::BylineBuilder.build self
     end
 
+    Contract None => Bool
     def draft?
       pubdate.nil?
     end
 
+    Contract None => String
     def post_status
       pubdate ? 'public' : 'draft'
     end
 
+    # FIXME: What's wrong with this contract? Confident much?
+    Contract Maybe[String] => Maybe[ActiveSupport::TimeWithZone]
     def post_status=(new_status)
       return unless new_status
       self.pubdate = pubdate_for_status(new_status)
     end
 
+    Contract None => String
     def pubdate_str
       return 'DRAFT' if draft?
       timestamp_for pubdate
     end
 
+    Contract None => Bool
     def published?
       !draft?
     end
 
     private
 
+    Contract String => Maybe[ActiveSupport::TimeWithZone]
     def pubdate_for_status(new_status)
       case new_status
       when 'public'
