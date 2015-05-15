9d6a308830ff35f3104f59a83659b52147e3f673
Jeff Dickey <jdickey@seven-sigma.com>
2015-05-15 16:34:57 +0800

    Add Contracts to `NavbarMenuForDetails` module.
    RSpec: 1005 examples, 0 failures, 1 pending
    RuboCop: 291 files inspected, no offenses detected
    Coverage: 2887 / 2906 LOC (99.35%) covered.

--- app/helpers/application_helper/build_menu_for/navbar_menu_for_details.rb
+++ app/helpers/application_helper/build_menu_for/navbar_menu_for_details.rb
@@ -1,13 +1,19 @@
 
+require 'contracts'
+
 module ApplicationHelper
   # Module containing the `#build_menu_for` application-helper method.
   module BuildMenuFor
     # Settings for MenuForBuilder to use when building a :navbar menu.
     module NavbarMenuForDetails
+      include Contracts
+
+      Contract None => String
       def container_classes
         'nav navbar-nav'
       end
 
+      Contract None => HashOf[Symbol, String]
       def separator_attrs
         { style: 'min-width: 3rem;' }
       end
