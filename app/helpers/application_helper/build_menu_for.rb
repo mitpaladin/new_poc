50db3ed8d0584b565e23e71a645a7374f7ac6959
Jeff Dickey <jdickey@seven-sigma.com>
2015-05-15 17:58:53 +0800

    Contracts added to `BuildMenuFor`.
    RSpec: 1004 examples, 0 failures, 1 pending
    RuboCop: 291 files inspected, no offenses detected
    Coverage: 2906 / 2925 LOC (99.35%) covered.

--- app/helpers/application_helper/build_menu_for.rb
+++ app/helpers/application_helper/build_menu_for.rb
@@ -1,20 +1,28 @@
 
+require 'contracts'
+
+require 'user'
 require_relative 'build_menu_for/menu_for_details_selector'
 
 module ApplicationHelper
   # Module containing the `#build_menu_for` application-helper method.
   module BuildMenuFor
+    include Contracts
+    include Contracts::Modules
+
+    Contract Symbol, RespondTo[:name, :profile] => String
     def build_menu_for(which, current_user)
       MenuForBuilder.new(self, which, current_user).markup
     end
 
-    private
-
     # Class wrapping logic of `#build_menu_for` application-helper function.
     class MenuForBuilder
+      include Contracts
       include Rails.application.routes.url_helpers
       attr_reader :markup
 
+      Contract RespondTo[:content_tag], Symbol,
+               RespondTo[:name, :profile] => MenuForBuilder
       def initialize(h, which, current_user)
         extend MenuForDetailsSelector.new.select(which)
         @h = h
@@ -24,18 +32,21 @@ module ApplicationHelper
                   else
                     build_html_for_registered_user
                   end
+        self
       end
 
       protected
 
       attr_reader :h
 
+      Contract Proc => String
       def build_container(&block)
         h.content_tag :ul, class: container_classes do
           instance_eval(&block)
         end
       end
 
+      Contract None => String
       def build_html_for_guest_user
         build_container do
           build_item_for 'Home', href: root_path
@@ -46,6 +57,7 @@ module ApplicationHelper
         end
       end
 
+      Contract None => String
       def build_html_for_registered_user
         build_container do
           build_item_for 'Home', href: root_path
@@ -59,6 +71,7 @@ module ApplicationHelper
         end
       end
 
+      Contract String, Maybe[Hash] => String
       def build_item_for(text, attrs = {})
         item = h.content_tag :li do
           link = h.content_tag :a, attrs do
@@ -69,6 +82,7 @@ module ApplicationHelper
         h.concat item
       end
 
+      Contract None => String
       def build_separator_item
         item = h.content_tag :li, separator_attrs do
           HTMLEntities.new.decode '&nbsp;'
@@ -76,23 +90,25 @@ module ApplicationHelper
         h.concat item
       end
 
+      Contract RespondTo[:name, :profile] => Entity::User
       def get_entity_for(user)
         return user if user.respond_to? :guest_user?
         UserFactory.create user.attributes.symbolize_keys
       end
 
+      Contract None => Hash
       def logout_params
-        ret = {
+        {
           href: '/sessions/current',
           rel:  'nofollow'
-        }
-        ret['data-method'] = 'delete'
-        ret
+        }.tap { |h| h['data-method'] = 'delete' }
       end
 
+      Contract None => String
       def user_profile_path
         user_path(id: @current_user.name.parameterize)
       end
     end # class MenuForBuilder
+    private_constant :MenuForBuilder
   end # module ApplicationHelper::BuildMenuFor
 end # module ApplicationHelper
