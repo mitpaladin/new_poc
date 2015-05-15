429c3ece1625ad61f478a7467ba4f13b799a894d
Jeff Dickey <jdickey@seven-sigma.com>
2015-05-15 16:04:19 +0800

    Added Contracts to `MenuForDetailsSelector`
    RSpec: 1005 examples, 0 failures, 1 pending
    RuboCop: 291 files inspected, no offenses detected
    Coverage: 2883 / 2902 LOC (99.35%) covered.

--- app/helpers/application_helper/build_menu_for/menu_for_details_selector.rb
+++ app/helpers/application_helper/build_menu_for/menu_for_details_selector.rb
@@ -1,12 +1,25 @@
 
+require 'contracts'
+
 require_relative 'navbar_menu_for_details'
 require_relative 'sidebar_menu_for_details'
 
 module ApplicationHelper
+  include Contracts
+  include Contracts::Modules
   # Module containing the `#build_menu_for` application-helper method.
   module BuildMenuFor
+    include Contracts
+    include Contracts::Modules
     # Which settings module should be used for a MenuForBuilder instance?
     class MenuForDetailsSelector
+      include Contracts
+      include Contracts::Modules
+
+      MENUBAR_SELECT_INPUT = Or[:navbar, :sidebar]
+      MENUBAR_CONTRACT_OUTPUT = RespondTo[:container_classes, :separator_attrs]
+
+      Contract MENUBAR_SELECT_INPUT => Any # MENUBAR_CONTRACT_OUTPUT
       def select(which)
         if which == :navbar
           NavbarMenuForDetails
