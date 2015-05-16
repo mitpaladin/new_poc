d1009ab617c667f4e208badb8918284adf827fcf
Jeff Dickey <jdickey@seven-sigma.com>
2015-05-17 03:07:13 +0800

    Added Contracts to `TimestampBuilder` module.
    RSpec: 998 examples, 0 failures, 1 pending
    RuboCop: 289 files inspected, no offenses detected
    Coverage: 3083 / 3102 LOC (99.39%) covered.

--- lib/timestamp_builder.rb
+++ lib/timestamp_builder.rb
@@ -1,11 +1,17 @@
 
+require 'contracts'
+
 # Simple and to the point.
 module TimestampBuilder
+  include Contracts
+
   # Provides uniform formatting for timestamps.
+  Contract Maybe[ActiveSupport::TimeWithZone] => String
   def timestamp_for(the_time = Time.zone.now)
     the_time.strftime timestamp_format
   end
 
+  Contract None => String
   def timestamp_format
     '%a %b %e %Y at %R %Z (%z)'
   end
