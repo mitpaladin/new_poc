0dfce9d84ab80843b9b6ab04c3bdbe01f7841fea
Jeff Dickey <jdickey@seven-sigma.com>
2015-05-17 01:07:27 +0800

    Added Contracts to `DataObjectFailure`.
    RSpec: 998 examples, 0 failures, 1 pending
    RuboCop: 289 files inspected, no offenses detected
    Coverage: 3033 / 3052 LOC (99.38%) covered.

--- lib/action_support/data_object_failure.rb
+++ lib/action_support/data_object_failure.rb
@@ -1,12 +1,19 @@
 
+require 'contracts'
+
 # Supporting code used by and for controller-namespaced Action classes.
 module ActionSupport
   # Build and raise a RuntimeError with YAML-encoded data as message.
   class DataObjectFailure
+    include Contracts
+
+    Contract RespondTo[:to_hash] => DataObjectFailure
     def initialize(options)
       @data = build_data_from_options options
+      self
     end
 
+    Contract None => AlwaysRaises
     def fail
       Kernel.fail YAML.dump(data)
     end
@@ -15,6 +22,7 @@ module ActionSupport
 
     attr_reader :data
 
+    Contract RespondTo[:to_hash] => HashOf[Symbol, Any]
     def build_data_from_options(options = {})
       messages = options.fetch :messages, []
       attributes = options.fetch :attributes, nil
