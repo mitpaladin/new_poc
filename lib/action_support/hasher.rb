2833bcf74608e437deeb8247ec597f720f6287e8
Jeff Dickey <jdickey@seven-sigma.com>
2015-05-17 01:57:31 +0800

    Contracts added to `Hasher`.
    RSpec: 998 examples, 0 failures, 1 pending
    RuboCop: 289 files inspected, no offenses detected
    Coverage: 3055 / 3074 LOC (99.38%) covered.

--- lib/action_support/hasher.rb
+++ lib/action_support/hasher.rb
@@ -1,13 +1,23 @@
 
+require 'contracts'
+
 module ActionSupport
   # Regularises Hashes and Rails' HashWithIndifferentAccess as used by actioms.
   class Hasher
+    include Contracts
+
     # Internal support methods for Hasher, since class methods can't be private.
     module Internals
+      include Contracts
+
+      DATA_CONTRACT = Or[RespondTo[:to_unsafe_h], RespondTo[:to_hash]]
+
+      Contract DATA_CONTRACT => RespondTo[:symbolize_keys]
       def self.convert_hash(data)
         data.send hasher_for(data)
       end
 
+      Contract DATA_CONTRACT => Symbol
       def self.hasher_for(data)
         return :to_unsafe_h if data.respond_to? :to_unsafe_h
         :to_h
@@ -15,6 +25,7 @@ module ActionSupport
     end
     private_constant :Internals
 
+    Contract Internals::DATA_CONTRACT => Hashlike
     def self.convert(data)
       Internals.convert_hash(data).symbolize_keys
     end
