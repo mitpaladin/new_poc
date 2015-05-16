d09beed632a2986a58153fd389819266d1959c69
Jeff Dickey <jdickey@seven-sigma.com>
2015-05-16 14:11:52 +0800

    Contracts added to `ProfilePostItemBuilder`.
    RSpec: 1004 examples, 0 failures, 1 pending
    RuboCop: 291 files inspected, no offenses detected
    Coverage: 2996 / 3015 LOC (99.37%) covered.

--- app/helpers/users_helper/profile_post_item_builder.rb
+++ app/helpers/users_helper/profile_post_item_builder.rb
@@ -1,11 +1,22 @@
 
+require 'contracts'
+
 # Builds the actual list item included in the list built by the
 # ProfileArticleListBuilder class.
 class ProfilePostItemBuilder
+  include Contracts
+
+  # FIXME: A few feature specs pass in helpers that Contracts cannot validate,
+  #        although they *seem* to work. Very odd.
+  FIXME_CONTRACT_INPUTS = Any, RespondTo[:pubdate_str, :slug]
+
+  Contract Any, RespondTo[:pubdate_str, :slug] => ProfilePostItemBuilder
   def initialize(h, post)
     @h, @post = h, post
+    self
   end
 
+  Contract None => String
   def to_html
     h.content_tag :li, nil, { class: 'list-group-item' }, false do
       h.concat build_link
@@ -18,12 +29,14 @@ class ProfilePostItemBuilder
 
   private
 
+  Contract None => String
   def build_link
     h.content_tag :a, nil, { href: "/posts/#{post.slug}" }, false do
       h.concat link_text
     end
   end
 
+  Contract None => String
   def link_text
     ["\"#{post.title}\"", 'Published', post.pubdate_str].join ' '
   end
