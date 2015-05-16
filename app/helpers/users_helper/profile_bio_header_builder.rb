d2fcbff66bc08331f6cb32848031a7caf31f5a92
Jeff Dickey <jdickey@seven-sigma.com>
2015-05-16 13:16:27 +0800

    Contracts added to `ProfileBioHeaderBuilder`
    RSpec: 1004 examples, 0 failures, 1 pending
    RuboCop: 291 files inspected, no offenses detected
    Coverage: 2974 / 2993 LOC (99.37%) covered.

--- app/helpers/users_helper/profile_bio_header_builder.rb
+++ app/helpers/users_helper/profile_bio_header_builder.rb
@@ -1,12 +1,19 @@
 
+require 'contracts'
+
 require 'current_user_identity'
 
 # Builds a header element. It contains a button if the named user is logged in.
 class ProfileBioHeaderBuilder
+  include Contracts
+
+  Contract String, RespondTo[:concat, :content_tag] => ProfileBioHeaderBuilder
   def initialize(user_name, h)
     @user_name, @h = user_name, h
+    self
   end
 
+  Contract None => String
   def to_html
     h.content_tag :h1, nil, { class: 'bio' }, false do
       h.concat "Profile Page for #{user_name}"
@@ -20,16 +27,19 @@ class ProfileBioHeaderBuilder
 
   private
 
+  Contract None => CurrentUserIdentity
   def identity
     @identity ||= CurrentUserIdentity.new(h.session)
   end
 
+  Contract None => RespondTo[:name]
   def current_user
     default = Struct.new(:name).new ''
     return default if identity.guest_user?
     identity.current_user
   end
 
+  Contract None => HashOf[Symbol, String]
   def link_attribs
     {
       class: 'btn btn-xs pull-right',
@@ -37,6 +47,7 @@ class ProfileBioHeaderBuilder
     }
   end
 
+  Contract None => String
   def make_button
     return '' unless current_user.name == user_name
     h.content_tag :a, nil, link_attribs, false do
