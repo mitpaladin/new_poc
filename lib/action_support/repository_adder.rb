d6221b3b546c87f9ebfc6c24651a179ace24f7cc
Jeff Dickey <jdickey@seven-sigma.com>
2015-05-17 02:03:56 +0800

    Added Contracts to `RepositoryAdder`.
    RSpec: 998 examples, 0 failures, 1 pending
    RuboCop: 289 files inspected, no offenses detected
    Coverage: 3061 / 3080 LOC (99.38%) covered.

--- lib/action_support/repository_adder.rb
+++ lib/action_support/repository_adder.rb
@@ -1,17 +1,30 @@
 
+require 'contracts'
+
 module ActionSupport
   # Adds a new record with specified attributes to a repository, using the
   # specified factory class to build an entity to pass to the repository.
   class RepositoryAdder
+    include Contracts
+
     attr_reader :entity
 
+    INIT_CONTRACT_INPUTS = {
+      attributes: RespondTo[:to_hash],
+      factory_class: Class,
+      repository: RespondTo[:add, :find_by_slug]
+    }
+
+    Contract INIT_CONTRACT_INPUTS => RepositoryAdder
     def initialize(attributes:, factory_class:, repository:)
       @attributes = attributes
       @factory_class = factory_class
       @repository = repository
       @persister_class = EntityPersister
+      self
     end
 
+    Contract None => RepositoryAdder
     def add
       params = { attributes: attributes, repository: repository }
       result = persister_class.new(params).persist do |attribs|
