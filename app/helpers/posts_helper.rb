2dca3c9d5fcf2ebf37708c7a1840e339456dfac2
Jeff Dickey <jdickey@seven-sigma.com>
2015-05-16 15:01:19 +0800

    Added Contracts to `PostsHelper`.
    RSpec: 1004 examples, 0 failures, 1 pending
    RuboCop: 291 files inspected, no offenses detected
    Coverage: 3006 / 3025 LOC (99.37%) covered.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,10 +1,15 @@
 
+require 'contracts'
+
 # for Decorations::Posts::HtmlBodyBuilder class.
 require 'posts/html_body_builder'
 require 'posts/byline_builder'
 
 # Old-style junk drawer of view-helper functions, etc.
 module PostsHelper
+  include Contracts
+
+  Contract Maybe[RespondTo[:to_hash]] => HashOf[Symbol, Any]
   def new_post_form_attributes(_params = {})
     attribs = shared_post_form_attributes 'new_post'
     attribs[:url] = posts_path
@@ -12,6 +17,7 @@ module PostsHelper
     attribs
   end
 
+  Contract RespondTo[:slug] => Hash
   def edit_post_form_attributes(post)
     attribs = shared_post_form_attributes 'edit_post'
     # NOTE: `post_path(post) works Just Fine in RSpec helper specs, but fails in
@@ -23,6 +29,7 @@ module PostsHelper
     attribs
   end
 
+  Contract RespondTo[:draft?] => String
   def status_select_options(post)
     option_items = [%w(draft draft), %w(public public)]
     # `status` appears to be an existing ActiveRecord::Base field. :(
@@ -30,6 +37,7 @@ module PostsHelper
     options_for_select option_items, current_status
   end
 
+  Contract Fixnum => ArrayOf[RespondTo[:author_name, :title]]
   def summarise_posts(count_in = 10)
     the_sorter = sorter_hack
     summ = PostsSummariser.new do |s|
@@ -41,6 +49,7 @@ module PostsHelper
 
   private
 
+  Contract String => Hash
   def shared_post_form_attributes(which)
     {
       html: {
@@ -51,21 +60,25 @@ module PostsHelper
     }
   end
 
+  Contract None => Proc
   def sorter_hack
     lambda do |data|
       drafts = data.select(&:draft?).sort_by do |post|
-        sort_by_timestamp post, :updated_at, :created_at
+        sort_by_timestamp post, [:updated_at, :created_at]
       end
       posts = data.select(&:published?).sort_by do |post|
-        sort_by_timestamp post, :pubdate
+        sort_by_timestamp post, [:pubdate]
       end
       [posts, drafts].flatten
     end
   end
 
-  def sort_by_timestamp(post, *fields)
+  SBT_CONTRACT_RETURN = [Symbol, ActiveSupport::TimeWithZone]
+
+  Contract RespondTo[:attributes], ArrayOf[Symbol] => SBT_CONTRACT_RETURN
+  def sort_by_timestamp(post, fields)
     selectors = post.attributes.to_hash.select { |k, _v| fields.include? k }
-    return Time.zone.now if selectors.empty?
+    return [:current, Time.zone.now] if selectors.empty?
     selectors.first
   end
 end
