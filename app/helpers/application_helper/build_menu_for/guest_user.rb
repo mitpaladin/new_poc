e11bebdd9c6049d017a05eadf99c0b1c162e3c3e
Jeff Dickey <jdickey@seven-sigma.com>
2015-05-23 04:06:57 +0800

    `ApplicationHelper::BuildMenuFor` cleaned up. [Close #283]
    `BuildMenuFor::GuestUser` now includes `BasicMenu`, completing the
    DRYing up of the two main `BuildMenuFor` menu classes.
    
    RSpec: 1032 examples, 0 failures, 1 pending
    RuboCop: 294 files inspected, no offenses detected
    Coverage: 3111 / 3125 LOC (99.55%) covered.

--- app/helpers/application_helper/build_menu_for/guest_user.rb
+++ app/helpers/application_helper/build_menu_for/guest_user.rb
@@ -1,60 +1,27 @@
 
 # require 'contracts'
 
-require_relative 'menu_for_details_selector'
+require_relative 'basic_menu'
 
 module ApplicationHelper
   # Module containing `ApplicationHelper#build_menu_for` and support classes.
   module BuildMenuFor
     class GuestUser
       include Contracts
+      include BasicMenu
 
       Contract ViewHelper, Or[:navbar, :sidebar] => GuestUser
       def initialize(h, which)
-        extend MenuForDetailsSelector.new.select(which)
-        @h = h
+        init_basic_menu h, which
         self
       end
 
       Contract None => String
       def markup
         build_container do
-          build_item_for 'Home', href: h.root_path
-          build_item_for 'All members', href: h.users_path
-          build_separator_item
           build_item_for 'Sign up', href: h.new_user_path
           build_item_for 'Log in', href: h.new_session_path
         end
-      end
-
-      private
-
-      attr_reader :h
-
-      Contract Proc => String
-      def build_container(&block)
-        h.content_tag :ul, class: container_classes do
-          instance_eval(&block)
-        end
-      end
-
-      Contract String, Maybe[Hash] => String
-      def build_item_for(text, attrs = {})
-        item = h.content_tag :li do
-          link = h.content_tag :a, attrs do
-            text
-          end
-          h.concat link
-        end
-        h.concat item
-      end
-
-      Contract None => String
-      def build_separator_item
-        item = h.content_tag :li, separator_attrs do
-          HTMLEntities.new.decode '&nbsp;'
-        end
-        h.concat item
       end
     end # class ApplicationHelper::BuildMenuFor::GuestUser
   end
--- spec/helpers/application_helper/build_menu_for/guest_user_spec.rb
+++ spec/helpers/application_helper/build_menu_for/guest_user_spec.rb
@@ -4,8 +4,21 @@ require 'spec_helper'
 require 'application_helper/build_menu_for/guest_user'
 
 describe ApplicationHelper::BuildMenuFor::GuestUser, type: :request do
+  let(:helper) do
+    Class.new do
+      include ActionView::Helpers
+      include ActionView::Context
+      include Rails.application.routes.url_helpers
+    end.new
+  end
+
   before :each do
     Ox.default_options = { encoding: 'UTF-8' }
+  end
+
+  it 'includes the BasicMenu module' do
+    obj = described_class.new helper, :navbar
+    expect(obj).to be_a ApplicationHelper::BuildMenuFor::BasicMenu
   end
 
   describe 'supports initialisation with' do
@@ -43,20 +56,7 @@ describe ApplicationHelper::BuildMenuFor::GuestUser, type: :request do
     end
   end # describe 'supports initialisation with'
 
-  # NOTE: Validating markup can be done in one of two ways: by matching against
-  # regexes or by parsing it with Ox, Nokogiri, et al, and walking the node tree
-  # handed back. While regex matching is (purportedly) *conceptually* simpler,
-  # it quickly devolves into esoteric, intricate hieroglyphs that require at
-  # least as much effort to produce or decode as the code they're supposedly
-  # helping to verify. No, thank you. Been there; done that; lived to tell.
   describe 'has a #markup method which' do
-    let(:helper) do
-      Class.new do
-        include ActionView::Helpers
-        include ActionView::Context
-        include Rails.application.routes.url_helpers
-      end.new
-    end
     let(:obj) { described_class.new helper, :sidebar }
     let(:markup) { obj.markup }
     let(:outer_node) { Ox.parse obj.markup }
@@ -73,41 +73,7 @@ describe ApplicationHelper::BuildMenuFor::GuestUser, type: :request do
           expect(outer_node.locate 'li').to eq outer_node.nodes
         end
 
-        describe 'within its first list item, an :a element that' do
-          let(:list_item) { outer_node.locate('li[0]').first }
-          let(:anchor) { list_item.locate('a').first }
-
-          it 'has the text "Home"' do
-            expect(anchor.text).to eq 'Home'
-          end
-
-          it 'links to the root path' do
-            expect(anchor[:href]).to eq root_path
-          end
-        end # describe 'within its first list item, an :a element that'
-
-        describe 'within its second list item, an :a element that' do
-          let(:list_item) { outer_node.locate('li[1]').first }
-          let(:anchor) { list_item.locate('a').first }
-
-          it 'has the text "All members"' do
-            expect(anchor.text).to eq 'All members'
-          end
-
-          it 'links to the users path' do
-            expect(anchor[:href]).to eq users_path
-          end
-        end # describe 'within its second list item, an :a element that'
-
-        describe 'within its third list item, a string that' do
-          let(:list_item) { outer_node.locate('li[2]').first }
-          let(:content) { list_item.nodes.first }
-
-          it 'is a nonbreaking space' do
-            expected = [0xA0]
-            expect(content.codepoints).to eq expected
-          end
-        end # describe 'within its third list item, a string that'
+        # First three list items are as standard for a BasicMenu; no specs here.
 
         describe 'within its fourth list item, an :a element that' do
           let(:list_item) { outer_node.locate('li[3]').first }
