9bda86259489c6f726814b606ea095006507d430
Jeff Dickey <jdickey@seven-sigma.com>
2015-05-26 03:00:37 +0800

    New spec for `IndexRowBuilder`
    Support class for `UsersHelper`.
    
    RSpec: 1048 examples, 0 failures, 1 pending
    RuboCop: 296 files inspected, no offenses detected
    Coverage: 3134 / 3147 LOC (99.59%) covered.

--- lib/app_contracts.rb
+++ lib/app_contracts.rb
@@ -6,4 +6,6 @@ ControllerInstance = Contracts::RespondTo[:view_cache_dependency]
 
 Hashlike = Contracts::RespondTo[:to_hash]
 
+UserInstance = Contracts::RespondTo[:created_at, :name, :slug]
+
 ViewHelper = Contracts::RespondTo[:content_tag]
