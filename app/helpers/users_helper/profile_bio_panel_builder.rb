963e6fbdaea338bd1f38466fd00de4310f7f92a0
Jeff Dickey <jdickey@seven-sigma.com>
2015-05-29 23:07:36 +0800

    `ProfileBioPanelBuilder` now uses `Services::OxBuilder`.
    Own, top-level class is gone.
    
    RSpec: 1062 examples, 0 failures, 1 pending
    RuboCop: 301 files inspected, no offenses detected
    Coverage: 3246 / 3259 LOC (99.6%) covered.

--- app/helpers/users_helper/profile_bio_panel_builder.rb
+++ app/helpers/users_helper/profile_bio_panel_builder.rb
@@ -1,49 +1,11 @@
 
 require 'contracts'
-
-class OxBuilder
-  include Contracts
-
-  Contract Maybe[Proc] => OxBuilder
-  def initialize
-    Ox.default_options = { indent: 0, encoding: 'UTF-8' }
-    self
-  end
-
-  Contract None => String
-  def dump
-    Ox.dump doc
-  end
-
-  private
-
-  Contract String, Maybe[Proc] => Ox::Element
-  def element(name)
-    Ox::Element.new(name).tap { |ret| yield ret if block_given? }
-  end
-
-  Contract String, String => Ox::Element
-  def link_to(text, url)
-    element('a').tap do |elem|
-      elem[:href] = url
-      elem << text
-    end
-  end
-
-  Contract None => Ox::Document
-  def doc
-    @doc ||= new_doc
-  end
-
-  Contract None => Ox::Document
-  def new_doc
-    Ox::Document.new
-  end
-end # class OxBuilder
+# FIXME: Forced `require_relative`.
+require_relative '../../services/ox_builder'
 
 # Builds a Bootstrap panel with a profile's biodata/"profile" information.
 class ProfileBioPanelBuilder
-  class Builder < OxBuilder
+  class Builder < Services::OxBuilder
     include Contracts
 
     attr_reader :outer_div
@@ -78,7 +40,7 @@ class ProfileBioPanelBuilder
 
     private
 
-    Contract None => Ox::Element
+    Contract None => ELEMENT_TYPE
     def panel_heading
       element('div').tap do |ph|
         ph[:class] = 'panel-heading'
@@ -89,7 +51,7 @@ class ProfileBioPanelBuilder
       end # div.panel-heading
     end
 
-    Contract None => Ox::Element
+    Contract None => ELEMENT_TYPE
     def wrap_profile
       element('div').tap do |pb|
         pb[:class] = 'panel-body'
@@ -102,7 +64,7 @@ class ProfileBioPanelBuilder
     attr_reader :default_outer_div_class, :user_profile
 
     # Called from #to_ox > #wrap_profile
-    Contract None => Ox::Element
+    Contract None => ELEMENT_TYPE
     def parse_profile
       Ox.parse user_profile
     rescue Ox::ParseError # no outer/leading element markup; content only
